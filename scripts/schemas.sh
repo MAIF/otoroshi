5f80e376e836e2ffe197e10e0aabf4b6d9366335
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2017-12-12 15:02:40 +0100

    Add exchange schema

--- scripts/schemas.sh
+++ scripts/schemas.sh
@@ -6,5 +6,6 @@ FILES="$LOCATION/documentation/src/main/paradox/schemas/*.ditaa"
 for f in $FILES
 do
   FILE=`echo $f | sed 's/ditaa/png/g' | sed 's/schemas/img/g'`
+  echo "java -jar $LOCATION/scripts/ditaa.jar -E -S -o $f $FILE"
 	java -jar $LOCATION/scripts/ditaa.jar -E -S -o $f $FILE
 done
