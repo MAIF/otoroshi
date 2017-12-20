708e0a5c35996855e99b74c76b722f1bd4d06c07
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2017-12-20 10:28:59 +0100

    Fix scripts for travis build

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Uploading otoroshicli"
+# curl -T ./clients/cli/target/release/otoroshicli -umathieuancelin:$BINTRAY_API_KEY https://api.bintray.com/content/mathieuancelin/otoroshi/linux-otoroshicli/lastest/linux-otoroshicli
+
+echo "Uploading otoroshi.jar"
 curl -T ./otoroshi/target/scala-2.11/otoroshi.jar -umathieuancelin:$BINTRAY_API_KEY https://api.bintray.com/content/mathieuancelin/otoroshi/otoroshi.jar/lastest/otoroshi.jar
-# curl -T ./clients/cli/target/release/otoroshicli -umathieuancelin:$BINTRAY_API_KEY https://api.bintray.com/content/mathieuancelin/otoroshi/otoroshicli/lastest/otoroshicli
