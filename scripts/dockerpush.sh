0114031937f4a26b2e13aeb28fd2db1aa5117566
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2017-12-24 11:16:00 +0100

    Add otoroshicli

--- scripts/dockerpush.sh
+++ scripts/dockerpush.sh
@@ -12,6 +12,11 @@ docker build -t otoroshi-tryout .
 docker tag otoroshi-tryout maif-docker-docker.bintray.io/otoroshi-tryout
 docker push maif-docker-docker.bintray.io/otoroshi-tryout
 
+cd $LOCATION/docker/otoroshicli
+docker build -t otoroshicli .
+docker tag otoroshicli maif-docker-docker.bintray.io/otoroshicli
+docker push maif-docker-docker.bintray.io/otoroshicli
+
 cd $LOCATION
 
 
