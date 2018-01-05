deaded3f6a04dbd7f6992582d4598eb344dbcdb7
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-01-05 10:45:43 +0100

    Upload only if on master and no PR

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -12,6 +12,13 @@
 # curl -T ./macos-otoroshicli -umathieuancelin:$BINTRAY_API_KEY -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: mac-otoroshicli' https://api.bintray.com/content/maif/binaries/mac-otoroshicli/latest/otoroshicli
 # curl -T ./win-otoroshicli.exe -umathieuancelin:$BINTRAY_API_KEY -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: win-otoroshicli' https://api.bintray.com/content/maif/binaries/win-otoroshicli/latest/otoroshicli.exe
 
-echo "Uploading otoroshi.jar"
-curl -T ./otoroshi/target/scala-2.11/otoroshi.jar -umathieuancelin:$BINTRAY_API_KEY -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: otoroshi.jar' https://api.bintray.com/content/maif/binaries/otoroshi.jar/latest/otoroshi.jar
-curl -T ./otoroshi/target/universal/otoroshi-1.0.0.zip -umathieuancelin:$BINTRAY_API_KEY -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: otoroshi-dist' https://api.bintray.com/content/maif/binaries/otoroshi-dist/latest/otoroshi-dist.zip
+echo "Will upload artifacts for $TRAVIS_BRANCH / $TRAVIS_PULL_REQUEST"
+if test "$TRAVIS_BRANCH" = "master"
+then
+  if test "$TRAVIS_PULL_REQUEST" = "false"
+  then
+    echo "Uploading otoroshi.jar"
+    curl -T ./otoroshi/target/scala-2.11/otoroshi.jar -umathieuancelin:$BINTRAY_API_KEY -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: otoroshi.jar' https://api.bintray.com/content/maif/binaries/otoroshi.jar/latest/otoroshi.jar
+    curl -T ./otoroshi/target/universal/otoroshi-1.0.0.zip -umathieuancelin:$BINTRAY_API_KEY -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: otoroshi-dist' https://api.bintray.com/content/maif/binaries/otoroshi-dist/latest/otoroshi-dist.zip
+  fi
+fi
