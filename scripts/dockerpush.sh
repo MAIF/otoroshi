4cbdfd63bf728f2fd826c2de1da91d3dbef5e644
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-01-08 15:14:44 +0100

    Cleanup

--- scripts/dockerpush.sh
+++ scripts/dockerpush.sh
@@ -9,11 +9,6 @@ docker build --no-cache -t otoroshi .
 docker tag otoroshi maif-docker-docker.bintray.io/otoroshi
 docker push maif-docker-docker.bintray.io/otoroshi
 
-# cd $LOCATION/docker/tryout
-# docker build -t otoroshi-tryout .
-# docker tag otoroshi-tryout maif-docker-docker.bintray.io/otoroshi-tryout
-# docker push maif-docker-docker.bintray.io/otoroshi-tryout
-
 cd $LOCATION/docker/otoroshicli
 docker build --no-cache -t otoroshicli .
 docker tag otoroshicli maif-docker-docker.bintray.io/otoroshicli
