6f0fc417ce97d512469bff743430d7f3789a6ee5
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-01-08 14:45:46 +0100

    Finish otoroshicli docker image

--- docker/otoroshicli/cli.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#LOCATION=`pwd`
-#docker run -v "$LOCATION:/usr/app" otoroshicli -h
-#docker run otoroshicli services -h
-docker run otoroshicli $@
--- scripts/dockerpush.sh
+++ scripts/dockerpush.sh
@@ -2,8 +2,9 @@
 
 LOCATION=`pwd`
 
-cd $LOCATION/docker/build
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD maif-docker-docker.bintray.io
+
+cd $LOCATION/docker/build
 docker build --no-cache -t otoroshi .
 docker tag otoroshi maif-docker-docker.bintray.io/otoroshi
 docker push maif-docker-docker.bintray.io/otoroshi
@@ -13,10 +14,10 @@ docker push maif-docker-docker.bintray.io/otoroshi
 # docker tag otoroshi-tryout maif-docker-docker.bintray.io/otoroshi-tryout
 # docker push maif-docker-docker.bintray.io/otoroshi-tryout
 
-# cd $LOCATION/docker/otoroshicli
-# docker build -t otoroshicli .
-# docker tag otoroshicli maif-docker-docker.bintray.io/otoroshicli
-# docker push maif-docker-docker.bintray.io/otoroshicli
+cd $LOCATION/docker/otoroshicli
+docker build --no-cache -t otoroshicli .
+docker tag otoroshicli maif-docker-docker.bintray.io/otoroshicli
+docker push maif-docker-docker.bintray.io/otoroshicli
 
 cd $LOCATION
 
