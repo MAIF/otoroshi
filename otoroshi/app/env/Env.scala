f751a553e4b635f9bb95df6177c12c2d565f8a8d
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-02-05 15:22:21 +0100

    Enhance performances

--- otoroshi/app/env/Env.scala
+++ otoroshi/app/env/Env.scala
@@ -135,9 +135,14 @@ class Env(val configuration: Configuration,
   lazy val privateAppsExecutionContext: ExecutionContext = ExecutionContext.fromExecutorService(
     Executors.newFixedThreadPool(procNbr + 1, factory("otoroshi-private-apps-requests"))
   )
-  lazy val pressureExecutionContext: ExecutionContext = ExecutionContext.fromExecutorService(
-    Executors.newFixedThreadPool(procNbr + 1, factory("otoroshi-pressure"))
+  lazy val pressureActorSystem = ActorSystem(
+    "otoroshi-pressure-system",
+    configuration
+      .getConfig("app.actorsystems.gateway")
+      .map(_.underlying)
+      .getOrElse(ConfigFactory.empty)
   )
+  lazy val pressureExecutionContext: ExecutionContext = pressureActorSystem.dispatcher
 
   lazy val gatewayActorSystem = ActorSystem(
     "otoroshi-gateway-system",
@@ -247,6 +252,7 @@ class Env(val configuration: Configuration,
     internalActorSystem.terminate()
     // redisActorSystem.terminate()
     gatewayActorSystem.terminate()
+    pressureActorSystem.terminate()
     kafkaActorSytem.terminate()
     statsdActorSytem.terminate()
     datastores.after(configuration, environment, lifecycle)
