fb9974ec8bc366aea86daa2259c51dcb4a8bd953
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-04-23 15:51:32 +0200

    Try to fix JDK9 build

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd ./otoroshi
-./bin/otoroshi -Dhttp.port=8080 "$@" -XX:+IgnoreUnrecognizedVMOptions --add-modules=java.xml.bind --illegal-access=warn
+export JAVA_OPTS="$JAVA_OPTS -XX:+IgnoreUnrecognizedVMOptions --add-modules=java.xml.bind --illegal-access=warn"
+./bin/otoroshi -Dhttp.port=8080 "$@"
