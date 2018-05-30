00285fdfd4275b9d25d69f1466a14fe477c0a243
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-05-30 17:11:25 +0200

    Use wrk2 instead of wrk

--- docker/bench/client/client.sh
+++ docker/bench/client/client.sh
@@ -1,8 +1,8 @@
 echo "Sleeping"
 sleep 20
 echo "Warm up ..."
-wrk -t6 -c200 -d40s -H "Host: test.foo.bar" --latency http://otoroshi:8091/
+wrk -R 1000 -t6 -c200 -d40s -H "Host: test.foo.bar" --latency http://otoroshi:8091/
 echo "Warm up done !"
 sleep 10
 echo "Bench ..."
-wrk -t80 -c800 -d60s -H "Host: test.foo.bar" --latency http://otoroshi:8091/
+wrk -R 8000 -t80 -c800 -d60s -H "Host: test.foo.bar" --latency http://otoroshi:8091/
