5e339c10ac48810d2c984ae58df384cb83978fa9
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-05-31 12:06:31 +0200

    Add graalvm in bench

--- docker/bench/client/client.sh
+++ docker/bench/client/client.sh
@@ -6,3 +6,11 @@ echo "Warm up done !"
 sleep 10
 echo "Bench ..."
 wrk -R 8000 -t80 -c800 -d60s -H "Host: test.foo.bar" --latency http://otoroshi:8091/
+
+echo "Testing on graal VM"
+echo "Warm up ..."
+wrk -R 2000 -t6 -c200 -d40s -H "Host: test.foo.bar" --latency http://otoroshigraalvm:8092/
+echo "Warm up done !"
+sleep 10
+echo "Bench ..."
+wrk -R 8000 -t80 -c800 -d60s -H "Host: test.foo.bar" --latency http://otoroshigraalvm:8092/
