42a6113bb7745af7b390f44cc6b0a04998d1f6b8
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-06-04 17:07:45 +0200

    Add script to quickly setup benchmark test

---
+++ scripts/docker-bench.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# git clone https://github.com/MAIF/otoroshi.git otoroshi && cd ./otoroshi && sh ./scripts/docker-bench.sh
+# 
+# or
+# 
+# mkdir -p /tmp/otoroshi-bench
+# cd /tmp/otoroshi-bench
+# git clone https://github.com/MAIF/otoroshi.git otoroshi
+# cd otoroshi
+
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     git \
+     gnupg2 \
+     software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo docker run hello-world
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
+cd ./docker/bench
+sudo docker-compose build
+sudo docker-compose up
+
