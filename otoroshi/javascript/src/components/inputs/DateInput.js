585670f85a20d6974d6efad2677ef1168660e84e
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2019-08-02 15:23:20 +0200

    Fix #328

---
+++ otoroshi/javascript/src/components/inputs/DateInput.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Help } from './Help';
+
+import moment from 'moment';
+
+import { OtoDateTimePicker } from '../datepicker';
+
+export class DateTimeInput extends Component {
+  
+  onChange = e => {
+    if (e && e.preventDefault) e.preventDefault();
+    this.props.onChange(e);
+  };
+
+  render() {
+    if (this.props.hide) {
+      return null;
+    }
+    return (
+      <div className="form-group">
+        <label htmlFor={`input-${this.props.label}`} className="col-xs-12 col-sm-2 control-label">
+          {this.props.label} <Help text={this.props.help} />
+        </label>
+        <div className="col-sm-10" style={{ display: 'flex' }}>
+          <OtoDateTimePicker date={moment(this.props.value)} onChange={this.onChange} />
+        </div>
+      </div>
+    );
+  }
+}
