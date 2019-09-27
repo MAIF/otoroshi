170ca241e4ec28c5349267cb74c66c50c8f776d2
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2019-09-27 16:22:17 +0200

    Update version to 1.4.13-dev

--- otoroshi/app/env/Env.scala
+++ otoroshi/app/env/Env.scala
@@ -517,7 +517,7 @@ class Env(val configuration: Configuration,
     removeHeadersOut = Seq.empty,
   )
 
-  lazy val otoroshiVersion     = "1.4.12"
+  lazy val otoroshiVersion     = "1.4.13-dev"
   lazy val latestVersionHolder = new AtomicReference[JsValue](JsNull)
   lazy val checkForUpdates     = configuration.getOptional[Boolean]("app.checkForUpdates").getOrElse(true)
 
--- otoroshi/build.sbt
+++ otoroshi/build.sbt
@@ -1,6 +1,6 @@
 name := """otoroshi"""
 organization := "fr.maif.otoroshi"
-version := "1.4.12"
+version := "1.4.13-dev"
 scalaVersion := "2.12.8"
 
 lazy val root = (project in file("."))
