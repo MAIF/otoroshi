9209f44031b9e5f825dce73fefbd8878ecb48752
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2019-10-07 10:14:23 +0200

    Oooops

--- scripts/mtls-tests/test.sh
+++ scripts/mtls-tests/test.sh
@@ -9,7 +9,7 @@ cp ../../otoroshi/target/scala-2.12/otoroshi.jar ./otoroshi.jar
 java -Dapp.domain=oto.tools -jar otoroshi.jar &
 sleep 10
 yarn install
-node oto.sh
+node oto.js
 sleep 10
 go run clientfrontend.go > clientfrontend.out
 node check.js
