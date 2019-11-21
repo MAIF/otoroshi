624eecc4cca2b8d77a0a57713e9ba756b8bdc5d6
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2019-11-21 19:44:24 +0100

    Display doc

--- otoroshi/javascript/src/pages/ServicePage.js
+++ otoroshi/javascript/src/pages/ServicePage.js
@@ -1,4 +1,5 @@
 import React, { Component, Suspense } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import * as BackOfficeServices from '../services/BackOfficeServices';
 import { ServiceSidebar } from '../components/ServiceSidebar';
@@ -3078,42 +3079,86 @@ const converter = new showdown.Converter({
 });
 
 class PluginsDescription extends Component {
+
   state = { scripts: [] }
 
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
   componentDidMount() {
+    this.interval = setInterval(this.update, 2000);
     fetch(`/bo/api/proxy/api/scripts/_list?type=transformer`, {
       method: 'GET',
       credentials: 'include',
       headers: {
         'Accept': 'application/json'
       }
-    }).then(r => r.json()).then(scripts => this.setState({ scripts }));
+    }).then(r => r.json()).then(scripts => this.setState({ scripts: scripts.filter(e => !!e.description) }));
+  }
+
+  update = () => {
+    window.$('pre code').each((i, block) => {
+      window.hljs.highlightBlock(block);
+    });
+    const found = this.props.refs.map(this.find).filter(e => !!e).map(script => (
+      this.findNode(
+        script, 
+        null,
+        true
+      )
+    )).filter(e => !!e);
+    if (found.length > 0) {
+      this.forceUpdate();
+    }
+  }
+
+  find = (ref) => {
+    return this.state.scripts.filter(s => s.id === ref)[0];
+  }
+
+  findNode = (ref, tree, findOnly) => {
+    const nodes = Array.from(document.querySelectorAll('.Select-value-label'));
+    const node = nodes.filter(n => {
+      // console.log(`${n.innerText} === ${ref.name}`)
+      return n.innerText === ref.name
+    })[0];
+    // console.log(node)
+    if (!node) {
+      return null;
+    };
+    if (findOnly) {
+      return true;
+    }
+    const parentNode = node.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+    return ReactDOM.createPortal(tree, parentNode);
   }
 
   render() {
-    setTimeout(() => {
-      window.$('pre code').each((i, block) => {
-        window.hljs.highlightBlock(block);
-      });
-    });
     return (
       <>
-        {this.state.scripts.filter(e => !!e.description).map(script => (
-          <div className="form-group">
-          <label className="col-xs-12 col-sm-2 control-label" />
-          <div className="col-sm-10">
-            <div
-              style={{
-                padding: 10,
-                borderRadius: 5,
-                backgroundColor: '#494948',
-                width: '100%',
-              }}>
-              <h3>{script.name}</h3>
-              <p style={{ textAlign: 'justify' }} dangerouslySetInnerHTML={{ __html: converter.makeHtml(script.description) }} />
+        {this.props.refs.map(this.find).filter(e => !!e).map(script => (
+          this.findNode(
+            script, 
+            <div className="form-group">
+              <label className="col-xs-12 col-sm-2 control-label" />
+              <div className="col-sm-10">
+                <div
+                  className="plugin-doc"
+                  style={{
+                    padding: 10,
+                    borderRadius: 5,
+                    backgroundColor: '#494948',
+                    width: '100%',
+                  }}>
+                  <h3>{script.name}</h3>
+                  <p style={{ textAlign: 'justify' }} dangerouslySetInnerHTML={{ __html: converter.makeHtml(script.description) }} />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          )
         ))}
       </>
     );
