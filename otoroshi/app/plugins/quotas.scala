37ba52c5c8e3b4852d17f1b91273733c7d7d7ae9
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2020-02-10 11:12:11 +0100

    Fix #438

--- otoroshi/app/plugins/quotas.scala
+++ otoroshi/app/plugins/quotas.scala
@@ -1,7 +1,9 @@
 package otoroshi.plugins.quotas
 
 import akka.http.scaladsl.util.FastFuture
+import akka.http.scaladsl.util.FastFuture._
 import env.Env
+import models.{RemainingQuotas, ServiceDescriptor}
 import org.joda.time.DateTime
 import otoroshi.script.{AccessContext, AccessValidator}
 import play.api.libs.json.{JsObject, Json}
@@ -91,6 +93,116 @@ class InstanceQuotas extends AccessValidator {
       } else {
         FastFuture.successful(true)
       }
+    }
+  }
+}
+
+case class ServiceQuotasConfig(
+  throttlingQuota: Long = RemainingQuotas.MaxValue,
+  dailyQuota: Long = RemainingQuotas.MaxValue,
+  monthlyQuota: Long = RemainingQuotas.MaxValue
+)
+
+class ServiceQuotas extends AccessValidator {
+
+  override def name: String = "Public quotas"
+
+  override def defaultConfig: Option[JsObject] =
+    Some(
+      Json.obj(
+        "ServiceQuotas" -> Json.obj(
+          "throttlingQuota" -> 100,
+          "dailyQuota" -> RemainingQuotas.MaxValue,
+          "monthlyQuota" -> RemainingQuotas.MaxValue
+        )
+      )
+    )
+
+  override def description: Option[String] = Some(s"""This plugin will enforce public quotas on the current service
+                                                     |
+                                                     |This plugin can accept the following configuration
+                                                     |
+                                                     |```json
+                                                     |{
+                                                     |  "ServiceQuotas": {
+                                                     |    "throttlingQuota": 100,
+                                                     |    "dailyQuota": 10000000,
+                                                     |    "monthlyQuota": 10000000
+                                                     |  }
+                                                     |}
+                                                     |```""")
+
+  private def totalCallsKey(name: String)(implicit env: Env): String   = s"${env.storageRoot}:plugins:services-public-quotas:global:$name"
+  private def dailyQuotaKey(name: String)(implicit env: Env): String   = s"${env.storageRoot}:plugins:services-public-quotas:daily:$name"
+  private def monthlyQuotaKey(name: String)(implicit env: Env): String = s"${env.storageRoot}:plugins:services-public-quotas:monthly:$name"
+  private def throttlingKey(name: String)(implicit env: Env): String   = s"${env.storageRoot}:plugins:services-public-quotas:second:$name"
+
+  private def updateQuotas(descriptor: ServiceDescriptor, qconf: ServiceQuotasConfig, increment: Long = 1L)(implicit ec: ExecutionContext, env: Env): Future[Unit] = {
+    val dayEnd     = DateTime.now().secondOfDay().withMaximumValue()
+    val toDayEnd   = dayEnd.getMillis - DateTime.now().getMillis
+    val monthEnd   = DateTime.now().dayOfMonth().withMaximumValue().secondOfDay().withMaximumValue()
+    val toMonthEnd = monthEnd.getMillis - DateTime.now().getMillis
+    env.clusterAgent.incrementApi(descriptor.id, increment)
+    for {
+      _        <- env.datastores.rawDataStore.incrby(totalCallsKey(descriptor.id), increment)
+      secCalls <- env.datastores.rawDataStore.incrby(throttlingKey(descriptor.id), increment)
+      secTtl <- env.datastores.rawDataStore.pttl(throttlingKey(descriptor.id)).filter(_ > -1).recoverWith {
+        case _ => env.datastores.rawDataStore.pexpire(throttlingKey(descriptor.id), env.throttlingWindow * 1000)
+      }
+      dailyCalls <- env.datastores.rawDataStore.incrby(dailyQuotaKey(descriptor.id), increment)
+      dailyTtl <- env.datastores.rawDataStore.pttl(dailyQuotaKey(descriptor.id)).filter(_ > -1).recoverWith {
+        case _ => env.datastores.rawDataStore.pexpire(dailyQuotaKey(descriptor.id), toDayEnd.toInt)
+      }
+      monthlyCalls <- env.datastores.rawDataStore.incrby(monthlyQuotaKey(descriptor.id), increment)
+      monthlyTtl <- env.datastores.rawDataStore.pttl(monthlyQuotaKey(descriptor.id)).filter(_ > -1).recoverWith {
+        case _ => env.datastores.rawDataStore.pexpire(monthlyQuotaKey(descriptor.id), toMonthEnd.toInt)
+      }
+    } yield ()
+      // RemainingQuotas(
+      //   authorizedCallsPerSec = qconf.throttlingQuota,
+      //   currentCallsPerSec = (secCalls / env.throttlingWindow).toInt,
+      //   remainingCallsPerSec = qconf.throttlingQuota - (secCalls / env.throttlingWindow).toInt,
+      //   authorizedCallsPerDay = qconf.dailyQuota,
+      //   currentCallsPerDay = dailyCalls,
+      //   remainingCallsPerDay = qconf.dailyQuota - dailyCalls,
+      //   authorizedCallsPerMonth = qconf.monthlyQuota,
+      //   currentCallsPerMonth = monthlyCalls,
+      //   remainingCallsPerMonth = qconf.monthlyQuota - monthlyCalls
+      // )
+  }
+
+  private def withingQuotas(descriptor: ServiceDescriptor, qconf: ServiceQuotasConfig)(implicit ec: ExecutionContext, env: Env): Future[Boolean] =
+    for {
+      sec <- withinThrottlingQuota(descriptor, qconf)
+      day <- withinDailyQuota(descriptor, qconf)
+      mon <- withinMonthlyQuota(descriptor, qconf)
+    } yield sec && day && mon
+
+  private def withinThrottlingQuota(descriptor: ServiceDescriptor, qconf: ServiceQuotasConfig)(implicit ec: ExecutionContext, env: Env): Future[Boolean] =
+    env.datastores.rawDataStore
+      .get(throttlingKey(descriptor.id))
+      .fast
+      .map(_.map(_.utf8String.toLong).getOrElse(0L) <= (qconf.throttlingQuota * env.throttlingWindow))
+
+  private def withinDailyQuota(descriptor: ServiceDescriptor, qconf: ServiceQuotasConfig)(implicit ec: ExecutionContext, env: Env): Future[Boolean] =
+    env.datastores.rawDataStore.get(dailyQuotaKey(descriptor.id)).fast.map(_.map(_.utf8String.toLong).getOrElse(0L) < qconf.dailyQuota)
+
+  private def withinMonthlyQuota(descriptor: ServiceDescriptor, qconf: ServiceQuotasConfig)(implicit ec: ExecutionContext, env: Env): Future[Boolean] =
+    env.datastores.rawDataStore
+      .get(monthlyQuotaKey(descriptor.id))
+      .fast
+      .map(_.map(_.utf8String.toLong).getOrElse(0L) < qconf.monthlyQuota)
+
+  def canAccess(ctx: AccessContext)(implicit env: Env, ec: ExecutionContext): Future[Boolean] = {
+    val config = ctx.configFor("ServiceQuotas")
+    val qconf = ServiceQuotasConfig(
+      throttlingQuota = (config \ "throttlingQuota").asOpt[Long].getOrElse(RemainingQuotas.MaxValue),
+      dailyQuota = (config \ "dailyQuota").asOpt[Long].getOrElse(RemainingQuotas.MaxValue),
+      monthlyQuota = (config \ "monthlyQuota").asOpt[Long].getOrElse(RemainingQuotas.MaxValue)
+    )
+    withingQuotas(ctx.descriptor, qconf).flatMap {
+      case true => updateQuotas(ctx.descriptor, qconf).map(_ => true)
+      case false => FastFuture.successful(false)
     }
   }
 }
