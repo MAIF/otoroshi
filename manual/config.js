7c18f8d393650c1eea21d83ce4851c55988886cc
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2020-02-21 15:06:38 +0100

    Add reference config

---
+++ manual/config.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const src = __dirname + '/src/main/paradox/snippets/reference.conf';
+const dest = __dirname + '/src/main/paradox/snippets/reference-env.conf';
+
+const content = fs.readFileSync(src).toString('utf8');
+const finalContent = content.split('\n').filter(l => l.indexOf('{') > -1 || l.indexOf('}') > -1 || l.indexOf('${?') > -1).join('\n');
+fs.writeFileSync(dest, finalContent);
