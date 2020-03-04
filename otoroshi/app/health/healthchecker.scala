0e87d8359033cbb44b730799ee308599830e0f5d
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2020-03-04 12:51:00 +0100

    Fix #447

--- otoroshi/app/health/healthchecker.scala
+++ otoroshi/app/health/healthchecker.scala
@@ -3,13 +3,14 @@ package health
 import java.util.concurrent.TimeUnit
 
 import akka.actor.{Actor, Props}
+import akka.http.scaladsl.util.FastFuture
+import akka.stream.scaladsl.{Keep, Sink, Source}
 import env.Env
 import events.HealthCheckEvent
-import models.ServiceDescriptor
+import models.{ServiceDescriptor, Target}
 import org.joda.time.DateTime
 import play.api.Logger
 import security.{IdGenerator, OtoroshiClaim}
-import utils.http.MtlsConfig
 
 import scala.concurrent.Future
 import scala.concurrent.duration.Duration
@@ -32,86 +33,101 @@ class HealthCheckerActor()(implicit env: Env) extends Actor {
 
   lazy val logger = Logger("otoroshi-health-checker")
 
-  def checkService(desc: ServiceDescriptor): Future[Unit] =
-    Future
-      .sequence(desc.targets.map { target =>
-        val url   = s"${target.scheme}://${target.host}${desc.healthCheck.url}"
-        val start = System.currentTimeMillis()
-        val state = IdGenerator.extendedToken(128)
-        val value = env.snowflakeGenerator.nextIdStr()
-        val claim = OtoroshiClaim(
-          iss = env.Headers.OtoroshiIssuer,
-          sub = "HealthChecker",
-          aud = desc.name,
-          exp = DateTime.now().plusSeconds(30).toDate.getTime,
-          iat = DateTime.now().toDate.getTime,
-          jti = IdGenerator.uuid
-        ).serialize(desc.secComSettings)(env)
-        env.MtlsWs
-          .url(url, target.mtlsConfig)
-          .withRequestTimeout(Duration(30, TimeUnit.SECONDS))
-          .withHttpHeaders(
-            env.Headers.OtoroshiState                -> state,
-            env.Headers.OtoroshiClaim                -> claim,
-            env.Headers.OtoroshiHealthCheckLogicTest -> value
-          )
-          .withMaybeProxyServer(
-            desc.clientConfig.proxy.orElse(env.datastores.globalConfigDataStore.latestSafe.flatMap(_.proxies.services))
-          )
-          .get()
-          .andThen {
-            case Success(res) => {
-              val checkDone =
-                res.header(env.Headers.OtoroshiHealthCheckLogicTestResult).exists(_.toLong == value.toLong + 42L)
-              val hce = HealthCheckEvent(
-                `@id` = value,
-                `@timestamp` = DateTime.now(),
-                `@serviceId` = desc.id,
-                `@service` = desc.name,
-                `@product` = desc.metadata.getOrElse("product", "--"),
-                url = url,
-                duration = System.currentTimeMillis() - start,
-                status = res.status,
-                logicCheck = checkDone,
-                error = None,
-                health = (res.status, checkDone) match {
-                  case (a, true) if a > 199 && a < 500  => Some("GREEN")
-                  case (a, false) if a > 199 && a < 500 => Some("YELLOW")
-                  case _                                => Some("RED")
-                }
-              )
-              hce.toAnalytics()
-              hce.pushToRedis()
-              env.datastores.globalConfigDataStore.singleton().map { config =>
-                env.metrics.markString(s"services.${desc.id}.health", hce.health.getOrElse("RED"))
-              }
-              res.ignore()
+  def checkTarget(desc: ServiceDescriptor, target: Target): Future[Unit] = {
+    val url = s"${target.scheme}://${target.host}${desc.healthCheck.url}"
+    val start = System.currentTimeMillis()
+    val state = IdGenerator.extendedToken(128)
+    val value = env.snowflakeGenerator.nextIdStr()
+    val claim = OtoroshiClaim(
+      iss = env.Headers.OtoroshiIssuer,
+      sub = "HealthChecker",
+      aud = desc.name,
+      exp = DateTime.now().plusSeconds(30).toDate.getTime,
+      iat = DateTime.now().toDate.getTime,
+      jti = IdGenerator.uuid
+    ).serialize(desc.secComSettings)(env) // TODO: handle protocol v2
+    env.MtlsWs
+      .url(url, target.mtlsConfig)
+      .withRequestTimeout(Duration(30, TimeUnit.SECONDS))
+      .withHttpHeaders(
+        env.Headers.OtoroshiState -> state, // TODO: handle new version
+        env.Headers.OtoroshiClaim -> claim,
+        env.Headers.OtoroshiHealthCheckLogicTest -> value
+      )
+      .withMaybeProxyServer(
+        desc.clientConfig.proxy.orElse(env.datastores.globalConfigDataStore.latestSafe.flatMap(_.proxies.services))
+      )
+      .get()
+      .andThen {
+        case Success(res) => {
+          val checkDone =
+            res.header(env.Headers.OtoroshiHealthCheckLogicTestResult).exists(_.toLong == value.toLong + 42L)
+          val hce = HealthCheckEvent(
+            `@id` = value,
+            `@timestamp` = DateTime.now(),
+            `@serviceId` = desc.id,
+            `@service` = desc.name,
+            `@product` = desc.metadata.getOrElse("product", "--"),
+            url = url,
+            duration = System.currentTimeMillis() - start,
+            status = res.status,
+            logicCheck = checkDone,
+            error = None,
+            health = (res.status, checkDone) match {
+              case (a, true) if a > 199 && a < 500 => Some("GREEN")
+              case (a, false) if a > 199 && a < 500 => Some("YELLOW")
+              case _ => Some("RED")
             }
-            case Failure(error) => {
-              // error.printStackTrace()
-              logger.error(s"Error while checking health of service '${desc.name}' at '${url}'", error)
-              val hce = HealthCheckEvent(
-                `@id` = value,
-                `@timestamp` = DateTime.now(),
-                `@serviceId` = desc.id,
-                `@service` = desc.name,
-                `@product` = desc.metadata.getOrElse("product", "--"),
-                url = url,
-                duration = System.currentTimeMillis() - start,
-                status = 0,
-                logicCheck = false,
-                error = Some(error.getMessage),
-                health = Some("RED")
-              )
-              hce.toAnalytics()
-              hce.pushToRedis()
-              env.datastores.globalConfigDataStore.singleton().map { config =>
-                env.metrics.markString(s"services.${desc.id}.health", hce.health.getOrElse("RED"))
-              }
-            }
+          )
+          hce.toAnalytics()
+          hce.pushToRedis()
+          env.datastores.globalConfigDataStore.singleton().map { config =>
+            env.metrics.markString(s"services.${desc.id}.health", hce.health.getOrElse("RED"))
           }
-      })
+          res.ignore()
+        }
+        case Failure(error) => {
+          // error.printStackTrace()
+          logger.error(s"Error while checking health of service '${desc.name}' at '${url}'", error)
+          val hce = HealthCheckEvent(
+            `@id` = value,
+            `@timestamp` = DateTime.now(),
+            `@serviceId` = desc.id,
+            `@service` = desc.name,
+            `@product` = desc.metadata.getOrElse("product", "--"),
+            url = url,
+            duration = System.currentTimeMillis() - start,
+            status = 0,
+            logicCheck = false,
+            error = Some(error.getMessage),
+            health = Some("RED")
+          )
+          hce.toAnalytics()
+          hce.pushToRedis()
+          env.datastores.globalConfigDataStore.singleton().map { config =>
+            env.metrics.markString(s"services.${desc.id}.health", hce.health.getOrElse("RED"))
+          }
+        }
+      }
       .map(_ => ())
+      .recover {
+        case e => ()
+      }
+  }
+
+
+  def checkService(desc: ServiceDescriptor): Future[Unit] = {
+    desc.exists().flatMap {
+      case false => FastFuture.successful(())
+      case true => {
+        Source(desc.targets.toList)
+          .mapAsync(1)(target => checkTarget(desc, target))
+          .toMat(Sink.ignore)(Keep.right)
+          .run()
+          .map(_ => ())
+      }
+    }
+  }
 
   override def receive: Receive = {
     case CheckFirstService(startedAt, services) if services.isEmpty => {
