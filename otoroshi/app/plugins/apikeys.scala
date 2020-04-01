5106c1ab4fa315c265f895adbd9032656b363ba7
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2020-04-01 22:23:17 +0200

    Cleanup

--- otoroshi/app/plugins/apikeys.scala
+++ otoroshi/app/plugins/apikeys.scala
@@ -219,7 +219,7 @@ class CertificateAsApikey extends PreRouting {
   }
 }
 
-case class ClientCredentialFlowBody(grantType: String, clientId: String, clientSecret: String, scope: String)
+case class ClientCredentialFlowBody(grantType: String, clientId: String, clientSecret: String, scope: Option[String])
 
 class ClientCredentialFlowExtractor extends PreRouting {
 
@@ -261,16 +261,18 @@ class ClientCredentialFlow extends RequestTransformer {
       Json.obj(
         "ClientCredentialFlow" -> Json.obj(
           "expiration"   -> 1.hour.toMillis,
-          "jwtToken"     -> true
+          "restrictToCurrentGroup" -> true,
+          "jwtToken"     -> true,
+          "rootPath" -> "/.well-known/otoroshi/oauth"
         )
       )
     )
 
   override def description: Option[String] =
     Some(
-      s"""This plugin enabless the oauth client credentials flow on a service and add an endpoint (`/oauth/token`) to create an access_token given a client id and secret.
+      s"""This plugin enabless the oauth client credentials flow on a service and add an endpoint (`/.well-known/otoroshi/oauth/token`) to create an access_token given a client id and secret.
          |If you don't want to have access_tokens as JWT tokens, don't forget to use `ClientCredentialFlowExtractor` pre-routing plugin.
-         |Don't forget to authorize access to `/oauth/token` in service settings (public paths)
+         |Don't forget to authorize access to `/.well-known/otoroshi/oauth/token` in service settings (public paths)
          |
          |```json
          |${Json.prettyPrint(defaultConfig.get)}
@@ -291,12 +293,13 @@ class ClientCredentialFlow extends RequestTransformer {
   }
 
   override def transformRequestWithCtx(ctx: TransformerRequestContext)(implicit env: Env, ec: ExecutionContext, mat: Materializer): Future[Either[Result, HttpRequest]] = {
+    val conf = ctx.configFor("ClientCredentialFlow")
+    val useJwtToken = (conf \ "jwtToken").asOpt[Boolean].getOrElse(true)
+    val restrictToCurrentGroup = (conf \ "restrictToCurrentGroup").asOpt[Boolean].getOrElse(true)
+    val expiration = (conf \ "expiration").asOpt[Long].map(_.millis).getOrElse(1.hour)
+    val rootPath = (conf \ "rootPath").asOpt[String].getOrElse("/.well-known/otoroshi/oauth")
     (ctx.rawRequest.method.toLowerCase(), ctx.rawRequest.path) match {
-      case ("post", "/oauth/token") => {
-
-        val conf = ctx.configFor("ClientCredentialFlow")
-        val useJwtToken = (conf \ "jwtToken").asOpt[Boolean].getOrElse(true)
-        val expiration = (conf \ "expiration").asOpt[Long].map(_.millis).getOrElse(1.hour)
+      case ("post", path) if path == s"$rootPath/token" => {
 
         awaitingRequests.get(ctx.snowflake).map { promise =>
 
@@ -311,7 +314,11 @@ class ClientCredentialFlow extends RequestTransformer {
 
           def handleTokenRequest(ccfb: ClientCredentialFlowBody): Future[Either[Result, HttpRequest]] = ccfb match {
             case ClientCredentialFlowBody("client_credentials", clientId, clientSecret, scope) => {
-              env.datastores.apiKeyDataStore.findAuthorizeKeyFor(clientId, ctx.descriptor.id).flatMap {
+              val possibleApiKey = if (restrictToCurrentGroup)
+                env.datastores.apiKeyDataStore.findAuthorizeKeyFor(clientId, ctx.descriptor.id)
+              else
+                env.datastores.apiKeyDataStore.findById(clientId)
+              possibleApiKey.flatMap {
                 case Some(apiKey) if apiKey.clientSecret == clientSecret && useJwtToken=> {
                   val accessToken = JWT.create()
                     .withExpiresAt(DateTime.now().plusMillis(expiration.toMillis.toInt).toDate)
@@ -321,28 +328,38 @@ class ClientCredentialFlow extends RequestTransformer {
                     .withSubject("Otoroshi")
                     .sign(Algorithm.HMAC512(apiKey.clientSecret))
                   // no refresh token possible because of https://tools.ietf.org/html/rfc6749#section-4.4.3
-                  Results.Ok(Json.obj(
-                    "access_token" -> accessToken,
-                    "token_type" -> "Bearer",
-                    "expires_in" -> expiration.toSeconds,
-                    "scope" -> scope
-                  )).leftf
+                  val pass = scope.map(s => apiKey.metadata.get("scope").exists(_.split(" ").toSeq.intersect(s.split(" ").toSeq).nonEmpty)).getOrElse(true)
+                  if (pass) {
+                    val scopeObj = scope.orElse(apiKey.metadata.get("scope")).map(v => Json.obj("scope" -> v)).getOrElse(Json.obj())
+                    Results.Ok(Json.obj(
+                      "access_token" -> accessToken,
+                      "token_type" -> "Bearer",
+                      "expires_in" -> expiration.toSeconds
+                    ) ++ scopeObj).leftf
+                  } else {
+                    Results.Forbidden(Json.obj("error" -> "access_denied", "error_description" -> s"Client has not been granted scopes: ${scope.get}")).leftf
+                  }
                 }
                 case Some(apiKey) if apiKey.clientSecret == clientSecret && !useJwtToken=> {
                   val randomToken = IdGenerator.token(64)
                   env.datastores.rawDataStore.set(s"${env.storageRoot}:plugins:client-credentials-flow:access-tokens:$randomToken", ByteString(apiKey.clientSecret), Some(expiration.toMillis)).map { _ =>
-                    Results.Ok(Json.obj(
-                      "access_token" -> randomToken,
-                      "token_type" -> "Bearer",
-                      "expires_in" -> expiration.toSeconds,
-                      "scope" -> ccfb.scope
-                    )).left
+                    val pass = scope.map(s => apiKey.metadata.get("scope").exists(_.split(" ").toSeq.intersect(s.split(" ").toSeq).nonEmpty)).getOrElse(true)
+                    if (pass) {
+                      val scopeObj = scope.orElse(apiKey.metadata.get("scope")).map(v => Json.obj("scope" -> v)).getOrElse(Json.obj())
+                      Results.Ok(Json.obj(
+                        "access_token" -> randomToken,
+                        "token_type" -> "Bearer",
+                        "expires_in" -> expiration.toSeconds
+                      ) ++ scopeObj).left
+                    } else {
+                      Results.Forbidden(Json.obj("error" -> "access_denied", "error_description" -> s"Client has not been granted scopes: ${scope.get}")).left
+                    }
                   }
                 }
-                case _ => Results.BadRequest(Json.obj("error" -> s"bad client_id or client_secret for ${ctx.snowflake}")).leftf
+                case _ => Results.Unauthorized(Json.obj("error" -> "access_denied", "error_description" -> s"Bad client credentials")).leftf
               }
             }
-            case _ => Results.BadRequest(Json.obj("error" -> s"bad grant_type for ${ctx.snowflake}")).leftf
+            case _ => Results.BadRequest(Json.obj("error" -> "unauthorized_client", "error_description" -> s"Grant type '${ccfb.grantType}' not supported !")).leftf
           }
 
           bodySource.runFold(ByteString.empty)(_ ++ _).flatMap { bodyRaw =>
@@ -358,7 +375,7 @@ class ClientCredentialFlow extends RequestTransformer {
                   body.get("client_secret"),
                   body.get("scope"),
                 ) match {
-                  case (Some(gtype), Some(clientId), Some(clientSecret), scope) => handleTokenRequest(ClientCredentialFlowBody(gtype, clientId, clientSecret, scope.getOrElse("all")))
+                  case (Some(gtype), Some(clientId), Some(clientSecret), scope) => handleTokenRequest(ClientCredentialFlowBody(gtype, clientId, clientSecret, scope))
                   case _ => ctx.request.headers
                     .get("Authorization")
                     .filter(_.startsWith("Basic "))
@@ -369,10 +386,10 @@ class ClientCredentialFlow extends RequestTransformer {
                     .map(_.split(":").toSeq)
                     .map(v => (v.head, v.last))
                     .map {
-                      case (clientId, clientSecret) => handleTokenRequest(ClientCredentialFlowBody(body.getOrElse("grant_type", "--"), clientId, clientSecret, "all"))
+                      case (clientId, clientSecret) => handleTokenRequest(ClientCredentialFlowBody(body.getOrElse("grant_type", "--"), clientId, clientSecret, None))
                     }
                     .getOrElse {
-                      Results.BadRequest(Json.obj("error" -> s"bad body for ${ctx.snowflake}")).leftf
+                      Results.Unauthorized(Json.obj("error" -> "access_denied", "error_description" -> s"Unauthorized")).leftf
                     }
                 }
               }
@@ -384,7 +401,7 @@ class ClientCredentialFlow extends RequestTransformer {
                   (json \ "client_secret").asOpt[String],
                   (json \ "scope").asOpt[String],
                 ) match {
-                  case (Some(gtype), Some(clientId), Some(clientSecret), scope) => handleTokenRequest(ClientCredentialFlowBody(gtype, clientId, clientSecret, scope.getOrElse("all")))
+                  case (Some(gtype), Some(clientId), Some(clientSecret), scope) => handleTokenRequest(ClientCredentialFlowBody(gtype, clientId, clientSecret, scope))
                   case _ => ctx.request.headers
                     .get("Authorization")
                     .filter(_.startsWith("Basic "))
@@ -395,21 +412,21 @@ class ClientCredentialFlow extends RequestTransformer {
                     .map(_.split(":").toSeq)
                     .map(v => (v.head, v.last))
                     .map {
-                      case (clientId, clientSecret) => handleTokenRequest(ClientCredentialFlowBody((json \ "grant_type").asOpt[String].getOrElse("--"), clientId, clientSecret, "all"))
+                      case (clientId, clientSecret) => handleTokenRequest(ClientCredentialFlowBody((json \ "grant_type").asOpt[String].getOrElse("--"), clientId, clientSecret, None))
                     }
                     .getOrElse {
-                      Results.BadRequest(Json.obj("error" -> s"bad body for ${ctx.snowflake}")).leftf
+                      Results.Unauthorized(Json.obj("error" -> "access_denied", "error_description" -> s"Unauthorized")).leftf
                     }
                 }
               }
-              case _ => Results.BadRequest(Json.obj("error" -> s"bad body for ${ctx.snowflake}")).leftf
+              case _ => Results.Unauthorized(Json.obj("error" -> "access_denied", "error_description" -> s"Unauthorized")).leftf
             }
           } andThen {
             case _ =>
               if (!consumed.get()) bodySource.runWith(Sink.ignore)
           }
         } getOrElse {
-          Results.BadRequest(Json.obj("error" -> s"no body promise found for ${ctx.snowflake}")).leftf
+          Results.Unauthorized(Json.obj("error" -> "access_denied", "error_description" -> s"Unauthorized")).leftf
         }
       }
       case _ => ctx.otoroshiRequest.rightf
