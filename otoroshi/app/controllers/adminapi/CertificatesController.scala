ef5dd6872b2b781882412c2eb229a5fd3a4351b6
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2020-04-22 12:08:49 +0200

    Migrate client cert validators

--- otoroshi/app/controllers/adminapi/CertificatesController.scala
+++ otoroshi/app/controllers/adminapi/CertificatesController.scala
@@ -3,24 +3,120 @@ package controllers.adminapi
 import actions.ApiAction
 import akka.http.scaladsl.util.FastFuture
 import env.Env
-import events._
 import play.api.Logger
 import play.api.libs.json._
-import play.api.mvc.{AbstractController, ControllerComponents}
-import security.IdGenerator
+import play.api.mvc.{AbstractController, ControllerComponents, RequestHeader}
 import ssl.Cert
-import utils.JsonPatchHelpers.patchJson
+import utils._
 
-import otoroshi.utils.syntax.implicits._
+import scala.concurrent.{ExecutionContext, Future}
 
-class CertificatesController(ApiAction: ApiAction, cc: ControllerComponents)(implicit env: Env)
-  extends AbstractController(cc) {
+class CertificatesController(val ApiAction: ApiAction, val cc: ControllerComponents)(implicit val env: Env)
+  extends AbstractController(cc) with BulkControllerHelper[Cert, JsValue] with CrudControllerHelper[Cert, JsValue] {
 
   implicit lazy val ec = env.otoroshiExecutionContext
   implicit lazy val mat = env.otoroshiMaterializer
 
   lazy val logger = Logger("otoroshi-certificates-api")
 
+  def renewCert(id: String) = ApiAction.async { ctx =>
+    env.datastores.certificatesDataStore.findById(id).map(_.map(_.enrich())).flatMap {
+      case None       => FastFuture.successful(NotFound(Json.obj("error" -> s"No Certificate found")))
+      case Some(cert) => cert.renew().map(c => Ok(c.toJson))
+    }
+  }
+
+  override def readEntity(json: JsValue): Either[String, Cert] = Cert._fmt.reads(json).asEither match {
+    case Left(e) => Left(e.toString())
+    case Right(r) => Right(r)
+  }
+
+  override def writeEntity(entity: Cert): JsValue = Cert._fmt.writes(entity)
+
+  override def findByIdOps(id: String)(implicit env: Env, ec: ExecutionContext): Future[Either[ApiError[JsValue], OptionalEntityAndContext[Cert]]] = {
+    env.datastores.certificatesDataStore.findById(id).map { opt =>
+      Right(OptionalEntityAndContext(
+        entity = opt,
+        action = "ACCESS_CERTIFICATE",
+        message = "User accessed a certificate",
+        metadata = Json.obj("CertId" -> id),
+        alert = "CertAccessed"
+      ))
+    }
+  }
+
+  override def findAllOps(req: RequestHeader)(implicit env: Env, ec: ExecutionContext): Future[Either[ApiError[JsValue], SeqEntityAndContext[Cert]]] = {
+    env.datastores.certificatesDataStore.findAll().map { seq =>
+      Right(SeqEntityAndContext(
+        entity = seq,
+        action = "ACCESS_ALL_CERTIFICATES",
+        message = "User accessed all certificates",
+        metadata = Json.obj(),
+        alert = "CertsAccessed"
+      ))
+    }
+  }
+
+  override def createEntityOps(entity: Cert)(implicit env: Env, ec: ExecutionContext): Future[Either[ApiError[JsValue], EntityAndContext[Cert]]] = {
+    env.datastores.certificatesDataStore.set(entity).map {
+      case true => {
+        Right(EntityAndContext(
+          entity = entity,
+          action = "CREATE_CERTIFICATE",
+          message = "User created a certificate",
+          metadata = entity.toJson.as[JsObject],
+          alert = "CertCreatedAlert"
+        ))
+      }
+      case false => {
+        Left(JsonApiError(
+          500,
+          Json.obj("error" -> "certificate not stored ...")
+        ))
+      }
+    }
+  }
+
+  override def updateEntityOps(entity: Cert)(implicit env: Env, ec: ExecutionContext): Future[Either[ApiError[JsValue], EntityAndContext[Cert]]] = {
+    env.datastores.certificatesDataStore.set(entity).map {
+      case true => {
+        Right(EntityAndContext(
+          entity = entity,
+          action = "UPDATE_CERTIFICATE",
+          message = "User updated a certificate",
+          metadata = entity.toJson.as[JsObject],
+          alert = "CertUpdatedAlert"
+        ))
+      }
+      case false => {
+        Left(JsonApiError(
+          500,
+          Json.obj("error" -> "certificate not stored ...")
+        ))
+      }
+    }
+  }
+
+  override def deleteEntityOps(id: String)(implicit env: Env, ec: ExecutionContext): Future[Either[ApiError[JsValue], NoEntityAndContext[Cert]]] = {
+    env.datastores.certificatesDataStore.delete(id).map {
+      case true => {
+        Right(NoEntityAndContext(
+          action = "DELETE_CERTIFICATE",
+          message = "User deleted a certificate",
+          metadata = Json.obj("CertId" -> id),
+          alert = "CertDeletedAlert"
+        ))
+      }
+      case false => {
+        Left(JsonApiError(
+          500,
+          Json.obj("error" -> "certificate not deleted ...")
+        ))
+      }
+    }
+  }
+
+  /*
   def createCert() = ApiAction.async(parse.json) { ctx =>
     val body: JsObject = (ctx.request.body \ "id").asOpt[String] match {
       case None    => ctx.request.body.as[JsObject] ++ Json.obj("id" -> IdGenerator.token(64))
@@ -162,13 +258,6 @@ class CertificatesController(ApiAction: ApiAction, cc: ControllerComponents)(imp
     }
   }
 
-  def renewCert(id: String) = ApiAction.async { ctx =>
-    env.datastores.certificatesDataStore.findById(id).map(_.map(_.enrich())).flatMap {
-      case None       => FastFuture.successful(NotFound(Json.obj("error" -> s"No Certificate found")))
-      case Some(cert) => cert.renew().map(c => Ok(c.toJson))
-    }
-  }
-
   def allCerts() = ApiAction.async { ctx =>
     val paginationPage: Int = ctx.request.queryString.get("page").flatMap(_.headOption).map(_.toInt).getOrElse(1)
     val paginationPageSize: Int =
@@ -236,4 +325,5 @@ class CertificatesController(ApiAction: ApiAction, cc: ControllerComponents)(imp
       }
     }
   }
+  */
 }
