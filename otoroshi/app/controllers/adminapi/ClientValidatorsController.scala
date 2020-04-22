8191d6636b82107fc9ac7fa637f55dc077de98ab
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2020-04-22 12:03:38 +0200

    Migrate client cert validators

--- otoroshi/app/controllers/adminapi/ClientValidatorsController.scala
+++ otoroshi/app/controllers/adminapi/ClientValidatorsController.scala
@@ -2,20 +2,109 @@ package controllers.adminapi
 
 import actions.ApiAction
 import env.Env
-import play.api.libs.json.{JsArray, JsObject, Json}
-import play.api.mvc.{AbstractController, ControllerComponents}
-import security.IdGenerator
+import play.api.libs.json.{JsObject, JsValue, Json}
+import play.api.mvc.{AbstractController, ControllerComponents, RequestHeader}
 import ssl.ClientCertificateValidator
+import utils._
 
-class ClientValidatorsController(ApiAction: ApiAction, cc: ControllerComponents)(
-  implicit env: Env
-) extends AbstractController(cc) {
+import scala.concurrent.{ExecutionContext, Future}
 
-  import utils.future.Implicits._
+class ClientValidatorsController(val ApiAction: ApiAction, val cc: ControllerComponents)(implicit val env: Env)
+  extends AbstractController(cc) with BulkControllerHelper[ClientCertificateValidator, JsValue] with CrudControllerHelper[ClientCertificateValidator, JsValue] {
 
   implicit lazy val ec  = env.otoroshiExecutionContext
   implicit lazy val mat = env.otoroshiMaterializer
 
+  override def readEntity(json: JsValue): Either[String, ClientCertificateValidator] = ClientCertificateValidator.fmt.reads(json).asEither match {
+    case Left(e) => Left(e.toString())
+    case Right(r) => Right(r)
+  }
+
+  override def writeEntity(entity: ClientCertificateValidator): JsValue = ClientCertificateValidator.fmt.writes(entity)
+
+  override def findByIdOps(id: String)(implicit env: Env, ec: ExecutionContext): Future[Either[ApiError[JsValue], OptionalEntityAndContext[ClientCertificateValidator]]] = {
+    env.datastores.clientCertificateValidationDataStore.findById(id).map { opt =>
+      Right(OptionalEntityAndContext(
+        entity = opt,
+        action = "ACCESS_CLIENT_CERT_VALIDATOR",
+        message = "User accessed a client cert. validator",
+        metadata = Json.obj("ClientCertificateValidatorId" -> id),
+        alert = "ClientCertificateValidatorAccessed"
+      ))
+    }
+  }
+
+  override def findAllOps(req: RequestHeader)(implicit env: Env, ec: ExecutionContext): Future[Either[ApiError[JsValue], SeqEntityAndContext[ClientCertificateValidator]]] = {
+    env.datastores.clientCertificateValidationDataStore.findAll().map { seq =>
+      Right(SeqEntityAndContext(
+        entity = seq,
+        action = "ACCESS_ALL_CLIENT_CERT_VALIDATORS",
+        message = "User accessed all client cert. validators",
+        metadata = Json.obj(),
+        alert = "ClientCertificateValidatorsAccessed"
+      ))
+    }
+  }
+
+  override def createEntityOps(entity: ClientCertificateValidator)(implicit env: Env, ec: ExecutionContext): Future[Either[ApiError[JsValue], EntityAndContext[ClientCertificateValidator]]] = {
+    env.datastores.clientCertificateValidationDataStore.set(entity).map {
+      case true => {
+        Right(EntityAndContext(
+          entity = entity,
+          action = "CREATE_CLIENT_CERT_VALIDATOR",
+          message = "User created a client cert. validator",
+          metadata = entity.asJson.as[JsObject],
+          alert = "ClientCertificateValidatorCreatedAlert"
+        ))
+      }
+      case false => {
+        Left(JsonApiError(
+          500,
+          Json.obj("error" -> "client cert. validator not stored ...")
+        ))
+      }
+    }
+  }
+
+  override def updateEntityOps(entity: ClientCertificateValidator)(implicit env: Env, ec: ExecutionContext): Future[Either[ApiError[JsValue], EntityAndContext[ClientCertificateValidator]]] = {
+    env.datastores.clientCertificateValidationDataStore.set(entity).map {
+      case true => {
+        Right(EntityAndContext(
+          entity = entity,
+          action = "UPDATE_CLIENT_CERT_VALIDATOR",
+          message = "User updated a client cert. validator",
+          metadata = entity.asJson.as[JsObject],
+          alert = "ClientCertificateValidatorUpdatedAlert"
+        ))
+      }
+      case false => {
+        Left(JsonApiError(
+          500,
+          Json.obj("error" -> "client cert. validator not stored ...")
+        ))
+      }
+    }
+  }
+
+  override def deleteEntityOps(id: String)(implicit env: Env, ec: ExecutionContext): Future[Either[ApiError[JsValue], NoEntityAndContext[ClientCertificateValidator]]] = {
+    env.datastores.clientCertificateValidationDataStore.delete(id).map {
+      case true => {
+        Right(NoEntityAndContext(
+          action = "DELETE_CLIENT_CERT_VALIDATOR",
+          message = "User deleted a client cert. validator",
+          metadata = Json.obj("ClientCertificateValidatorId" -> id),
+          alert = "ClientCertificateValidatorDeletedAlert"
+        ))
+      }
+      case false => {
+        Left(JsonApiError(
+          500,
+          Json.obj("error" -> "client cert. validator not deleted ...")
+        ))
+      }
+    }
+  }
+  /*
   def findAllClientValidators() = ApiAction.async { ctx =>
     env.datastores.clientCertificateValidationDataStore.findAll().map(all => Ok(JsArray(all.map(_.asJson))))
   }
@@ -80,5 +169,5 @@ class ClientValidatorsController(ApiAction: ApiAction, cc: ControllerComponents)
   def deleteClientValidator(id: String) = ApiAction.async { ctx =>
     env.datastores.clientCertificateValidationDataStore.delete(id).map(_ => Ok(Json.obj("deleted" -> true)))
   }
-
+*/
 }
