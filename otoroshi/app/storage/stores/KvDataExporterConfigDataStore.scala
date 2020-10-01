bfa66e0213f66bf1dc9f3df577b3d3e54e323037
Quentin <quentino.vega@gmail.com>
2020-10-01 16:56:39 +0200

    put back all mailer data exporter config templates

--- otoroshi/app/storage/stores/KvDataExporterConfigDataStore.scala
+++ otoroshi/app/storage/stores/KvDataExporterConfigDataStore.scala
@@ -105,6 +105,95 @@ class DataExporterConfigDataStore(redisCli: RedisLike, env: Env) extends RedisLi
           filtering = DataExporterConfigFiltering(),
           config = ConsoleMailerSettings()
         )
+      case Some("generic-mailer") =>
+        DataExporterConfig(
+          typ = DataExporterConfigType.Mailer,
+          id = IdGenerator.token,
+          name = "New generic mailer exporter config",
+          desc = "New generic mailer exporter config",
+          metadata = Map.empty,
+          enabled = false,
+          location = EntityLocation(),
+          groupSize = 25,
+          groupDuration = 60.seconds,
+          projection = Json.obj(),
+          filtering = DataExporterConfigFiltering(),
+          config = GenericMailerSettings(
+            url = "http://localhost:8080",
+            headers = Map.empty,
+            to = Seq.empty
+          )
+        )
+      case Some("mailgun") =>
+        DataExporterConfig(
+          typ = DataExporterConfigType.Mailer,
+          id = IdGenerator.token,
+          name = "New mailgun exporter config",
+          desc = "New mailgun exporter config",
+          metadata = Map.empty,
+          enabled = false,
+          location = EntityLocation(),
+          groupSize = 25,
+          groupDuration = 60.seconds,
+          projection = Json.obj(),
+          filtering = DataExporterConfigFiltering(),
+          config = MailgunSettings(
+            eu = true,
+            apiKey = "key",
+            domain = "domain",
+            to = Seq.empty
+          )
+        )
+      case Some("mailjet") =>
+        DataExporterConfig(
+          typ = DataExporterConfigType.Mailer,
+          id = IdGenerator.token,
+          name = "New mailjet exporter config",
+          desc = "New mailjet exporter config",
+          metadata = Map.empty,
+          enabled = false,
+          location = EntityLocation(),
+          groupSize = 25,
+          groupDuration = 60.seconds,
+          projection = Json.obj(),
+          filtering = DataExporterConfigFiltering(),
+          config = MailjetSettings(
+            apiKeyPublic = "key-public",
+            apiKeyPrivate = "key-private",
+            to = Seq.empty
+          )
+        )
+      case Some("none-mailer") =>
+        DataExporterConfig(
+          typ = DataExporterConfigType.Mailer,
+          id = IdGenerator.token,
+          name = "New none mailer exporter config",
+          desc = "New none mailer exporter config",
+          metadata = Map.empty,
+          enabled = false,
+          location = EntityLocation(),
+          projection = Json.obj(),
+          filtering = DataExporterConfigFiltering(),
+          config = NoneMailerSettings()
+        )
+      case Some("sendgrid") =>
+        DataExporterConfig(
+          typ = DataExporterConfigType.Mailer,
+          id = IdGenerator.token,
+          name = "New sendgrid mailer exporter config",
+          desc = "New sendgrid mailer exporter config",
+          metadata = Map.empty,
+          enabled = false,
+          location = EntityLocation(),
+          groupSize = 25,
+          groupDuration = 60.seconds,
+          projection = Json.obj(),
+          filtering = DataExporterConfigFiltering(),
+          config = SendgridSettings(
+            apiKey = "apikey",
+            to = Seq.empty
+          )
+        )
       case Some("file") =>
         DataExporterConfig(
           typ = DataExporterConfigType.File,
