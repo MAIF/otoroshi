405548d0c09952f1d2d364de35c917fd0e7b2218
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2020-11-17 09:34:43 +0100

    fix #683

---
+++ otoroshi/app/jobs/apikeysRotation.scala
@@ -0,0 +1,41 @@
+package jobs
+
+import akka.http.scaladsl.util.FastFuture
+import akka.stream.scaladsl.{Sink, Source}
+import env.Env
+import otoroshi.script.{Job, JobContext, JobId, JobInstantiation, JobKind, JobStarting, JobVisibility}
+import play.api.Logger
+import otoroshi.utils.syntax.implicits._
+
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
+
+class ApikeysSecretsRotationJobs extends Job {
+
+  private val logger = Logger("otoroshi-apikeys-secrets-rotation-job")
+
+  override def uniqueId: JobId = JobId("io.otoroshi.core.jobs.ApikeysSecretsRotationJobs")
+
+  override def name: String = "Otoroshi apikeys secrets rotation jobs"
+
+  override def visibility: JobVisibility = JobVisibility.Internal
+
+  override def kind: JobKind = JobKind.ScheduledEvery
+
+  override def initialDelay(ctx: JobContext): Option[FiniteDuration] = 1.minutes.some
+
+  override def interval(ctx: JobContext): Option[FiniteDuration] = 10.minutes.some
+
+  override def starting: JobStarting = JobStarting.Automatically
+
+  override def instantiation: JobInstantiation = JobInstantiation.OneInstancePerOtoroshiCluster
+
+  override def jobRun(ctx: JobContext)(implicit env: Env, ec: ExecutionContext): Future[Unit] = {
+    env.datastores.apiKeyDataStore.findAll().flatMap { apikeys =>
+      Source(apikeys.toList)
+        .mapAsync(1)(apikey => env.datastores.apiKeyDataStore.keyRotation(apikey))
+        .runWith(Sink.seq)(env.otoroshiMaterializer)
+        .map(_ => ())
+    }
+  }
+}
