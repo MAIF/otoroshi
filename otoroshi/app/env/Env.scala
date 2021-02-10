ce6ccd829716a49fe58cd17ca87dceafa93b0503
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2021-02-10 18:06:59 +0100

    Update version to 1.5.0-dev

--- otoroshi/app/env/Env.scala
+++ otoroshi/app/env/Env.scala
@@ -775,7 +775,7 @@ class Env(val configuration: Configuration,
     useAkkaHttpClient = true
   )
 
-  lazy val otoroshiVersion     = "1.5.0-alpha.7"
+  lazy val otoroshiVersion     = "1.5.0-dev"
   lazy val checkForUpdates     = configuration.getOptionalWithFileSupport[Boolean]("app.checkForUpdates").getOrElse(true)
 
   lazy val jmxEnabled = configuration.getOptionalWithFileSupport[Boolean]("otoroshi.jmx.enabled").getOrElse(false)
--- otoroshi/build.sbt
+++ otoroshi/build.sbt
@@ -1,6 +1,6 @@
 name := """otoroshi"""
 organization := "fr.maif.otoroshi"
-version := "1.5.0-alpha.7"
+version := "1.5.0-dev"
 scalaVersion := scalaLangVersion
 
 lazy val root = (project in file("."))
