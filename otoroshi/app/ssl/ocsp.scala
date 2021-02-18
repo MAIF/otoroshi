df9beb0bfc4d1d7fb5beec511803ef4448f8f197
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2021-02-18 09:24:37 +0100

    Add another command

--- otoroshi/app/ssl/ocsp.scala
+++ otoroshi/app/ssl/ocsp.scala
@@ -18,10 +18,11 @@ object OcspResponder {
 // check for inspiration: https://github.com/wdawson/revoker/blob/master/src/main/java/wdawson/samples/revoker/resources/OCSPResponderResource.java
 // for testing: https://akshayranganath.github.io/OCSP-Validation-With-Openssl/
 // test command: openssl ocsp -issuer chain.pem -cert certificate.pem -text -url http://otoroshi-api.oto.tools:9999/.well-known/otoroshi/ocsp -header "HOST" "otoroshi-api.oto.tools"
+// test command: openssl ocsp -issuer "ca.cer" -cert "*.oto.tools.cer" -text -url http://otoroshi-api.oto.tools:9999/.well-known/otoroshi/ocsp -header "HOST" "otoroshi-api.oto.tools"
 class OcspResponder(env: Env) {
 
-  val logger = Logger("otoroshi-ocsp-responder")
-  implicit val mat = env.otoroshiMaterializer
+  private val logger = Logger("otoroshi-ocsp-responder")
+  private implicit val mat = env.otoroshiMaterializer
 
   def respond(req: RequestHeader, body: Source[ByteString, _])(implicit ec: ExecutionContext): Future[Result] = {
     body.runFold(ByteString.empty)(_ ++ _).map { bs =>
