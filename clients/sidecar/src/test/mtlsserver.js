df7b62cf8fa4b9e14e16177969d74863a6ea9401
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2021-02-23 11:03:57 +0100

    More options for test

--- clients/sidecar/src/test/mtlsserver.js
+++ clients/sidecar/src/test/mtlsserver.js
@@ -8,7 +8,8 @@ function Backend(opts) {
     cert: opts.cert,
     ca: opts.ca,
     rejectUnauthorized: opts.rejectUnauthorized,
-    requestCert: opts.requestCert  
+    requestCert: opts.requestCert,
+    maxVersion: 'TLSv1.2'
   }, (req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify({ hello: 'hello' }));
@@ -16,16 +17,69 @@ function Backend(opts) {
   });
 
   return {
-    listen: (lopts) => {
-      return server.listen(lopts.port);
+    listen: (lopts, f) => {
+      return server.listen(lopts.port, f);
     }
   };
 }
+
+function client(mtls) {
+  return new Promise((success, failure) => {
+    const options = {
+      hostname: 'api-test.oto.tools',
+      port: 3333,
+      path: '/test',
+      method: 'GET',
+      headers: {
+        host: 'api-test.oto.tools',
+      },
+      ca: fs.readFileSync('./certs/ca.cer'),
+      key: mtls ? fs.readFileSync('./certs/client.key') : null,
+      cert: mtls ? fs.readFileSync('./certs/client.cer') : null
+    };
+
+    const request = https.request(options, (resp) => {
+      const response = {
+        statusCode: resp.statusCode,
+        headers: resp.headers,
+        body: ''
+      }
+      const isJson = (resp.headers['content-type'] || '').indexOf('application/json') > -1;
+      resp.on('data', (d) => {
+        response.body = response.body + d
+      });
+      resp.on('end', () => {
+        response.body = isJson ? JSON.parse(response.body) : response.body
+        console.log(JSON.stringify(response, null, 2));
+        success();
+      })
+    }).on('error', (e) => {
+      console.log('client error', e)
+      failure(e);
+    });
+    request.end();
+  });
+}
+
+const mtls = !process.argv.slice(2).includes('mtls_off');
+const noexit = process.argv.slice(2).includes('no_exit');
+const noclient = process.argv.slice(2).includes('no_client');
 
 Backend({
   key:  fs.readFileSync('./certs/backend.key'),
   cert: fs.readFileSync('./certs/backend.cer'),
   ca:   fs.readFileSync('./certs/ca.cer'),
-  rejectUnauthorized: true,
-  requestCert: true,
-}).listen({ port: 3333 })
+  rejectUnauthorized: mtls,
+  requestCert: mtls,
+}).listen({ port: 3333 }, () => {
+  console.log("server listening at https://127.0.0.1:3333")
+  console.log(`mtls is ${mtls ? 'on' : 'off'}`);
+  console.log('')
+  if (!noclient) {
+    setTimeout(() => {
+      client(mtls).then(() => {
+        if (!noexit) process.exit(0);
+      });
+    }, 2000)
+  }
+})
