9d6be95a73afd9f0086ad47900d5c6ea4fe0600c
Baudelot Philippe <philippe@baudelot.eu>
2021-02-25 10:41:26 +0100

    fix some margins

--- otoroshi/javascript/src/pages/CertificatesPage.js
+++ otoroshi/javascript/src/pages/CertificatesPage.js
@@ -809,7 +809,7 @@ export class CertificatesPage extends Component {
               <i className="fas fa-plus-circle" /> Let's Encrypt cert.
             </button>
           </div>*/}
-              <div className="mt-5 mb-5 btn__group">
+              <div className="mt-10 mb-5 btn__group">
               {/*<button
               type="button"
               onClick={this.createSelfSigned}
