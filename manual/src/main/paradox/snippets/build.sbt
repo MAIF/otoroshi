27f3412d1eb514bec9eeb2f7440cc4a4cf957bbc
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2021-08-30 15:25:40 +0200

    Update version to 1.5.0-dev

--- manual/src/main/paradox/snippets/build.sbt
+++ manual/src/main/paradox/snippets/build.sbt
@@ -1,2 +1,2 @@
-libraryDependencies += "fr.maif" %% "otoroshi" % "1.5.0-beta.4"
-libraryDependencies += "fr.maif" %% "otoroshi" % "1.5.0-beta.4" classifier "assets"
+libraryDependencies += "fr.maif" %% "otoroshi" % "1.5.0-dev"
+libraryDependencies += "fr.maif" %% "otoroshi" % "1.5.0-dev" classifier "assets"
--- otoroshi/app/env/Env.scala
+++ otoroshi/app/env/Env.scala
@@ -943,7 +943,7 @@ class Env(
     useAkkaHttpClient = true
   )
 
-  lazy val otoroshiVersion    = "1.5.0-beta.4"
+  lazy val otoroshiVersion    = "1.5.0-dev"
   lazy val otoroshiVersionSem = Version(otoroshiVersion)
   lazy val checkForUpdates    = configuration.getOptionalWithFileSupport[Boolean]("app.checkForUpdates").getOrElse(true)
 
