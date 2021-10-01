b4b8c94a6d7af1cd5d2cde35d1ded017d4ba1d38
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2021-10-01 15:29:26 +0200

    better content type handling

--- otoroshi/app/plugins/authcallers.scala
+++ otoroshi/app/plugins/authcallers.scala
@@ -3,6 +3,7 @@ package otoroshi.plugins.authcallers
 import akka.stream.Materializer
 import akka.util.ByteString
 import otoroshi.env.Env
+import otoroshi.models.ServiceDescriptor
 import otoroshi.script._
 import otoroshi.utils.http.MtlsConfig
 import otoroshi.utils.syntax.implicits._
@@ -30,6 +31,7 @@ case class OAuth2CallerConfig(
   method: String,
   headerName: String,
   headerValueFormat: String,
+  jsonPayload: Boolean,
   clientId: String,
   clientSecret: String,
   scope: Option[String],
@@ -51,6 +53,7 @@ object OAuth2CallerConfig {
       method = json.select("method").asOpt[String].getOrElse("POST"),
       headerName = json.select("headerName").asOpt[String].getOrElse("Authorization"),
       headerValueFormat = json.select("headerValueFormat").asOpt[String].getOrElse("Bearer %s"),
+      jsonPayload = json.select("jsonPayload").asOpt[Boolean].getOrElse(false),
       clientId = json.select("clientId").asOpt[String].getOrElse("--"),
       clientSecret = json.select("clientSecret").asOpt[String].getOrElse("--"),
       scope = json.select("scope").asOpt[String].filter(_.nonEmpty),
@@ -69,6 +72,7 @@ class OAuth2Caller extends RequestTransformer {
 
   override def description: Option[String] =
     s"""This plugin can be used to call api that are authenticated using OAuth2 client_credential/password flow.
+       |Do not forget to enable client retry to handle token generation on expire.
        |
        |This plugin accepts the following configuration
        |
@@ -81,6 +85,7 @@ class OAuth2Caller extends RequestTransformer {
     "method" -> "POST",
     "headerName" -> "Authorization",
     "headerValueFormat" -> "Bearer %s",
+    "jsonPayload" -> false,
     "clientId" -> "the client_id",
     "clientSecret" -> "the client_secret",
     "scope" -> "an optional scope",
@@ -94,22 +99,37 @@ class OAuth2Caller extends RequestTransformer {
   override def configRoot: Option[String] = "OAuth2Caller".some
 
   def getToken(key: String, config: OAuth2CallerConfig)(implicit env: Env, ec: ExecutionContext, mat: Materializer): Future[Either[(String, Int), String]] = {
-    val body = config.kind match {
+    val body: String = config.kind match {
+      case OAuth2Kind.ClientCredentials if config.jsonPayload => (Json.obj("client_id" -> config.clientId, "client_secret" -> config.clientSecret, "grant_type" -> "client_credentials").applyOnWithOpt(config.scope) { (json, scope) => json ++ Json.obj("scope" -> scope) }.applyOnWithOpt(config.audience) { (json, audience) => json ++ Json.obj("audience" -> audience) }).stringify
+      case OAuth2Kind.Password          if config.jsonPayload => (Json.obj("client_id" -> config.clientId, "client_secret" -> config.clientSecret, "grant_type" -> "password", "username" -> config.user.getOrElse("--"), "password" -> config.password.getOrElse("--")).applyOnWithOpt(config.scope) { (json, scope) => json ++ Json.obj("scope" -> scope) }.applyOnWithOpt(config.audience) { (json, audience) => json ++ Json.obj("audience" -> audience) }).stringify
       case OAuth2Kind.ClientCredentials => s"client_id=${config.clientId}&client_secret=${config.clientSecret}&grant_type=client_credentials${config.scope.map(s => s"&scope=$s").getOrElse("")}${config.audience.map(s => s"&audience=$s").getOrElse("")}"
       case OAuth2Kind.Password          => s"client_id=${config.clientId}&client_secret=${config.clientSecret}&grant_type=password&username=${config.user.getOrElse("--")}&password=${config.password.getOrElse("--")}${config.scope.map(s => s"&scope=$s").getOrElse("")}${config.audience.map(s => s"&audience=$s").getOrElse("")}"
     }
+    val ctype = if (config.jsonPayload) "application/json" else "application/x-www-form-urlencoded"
     env.MtlsWs.url(config.url, config.tlsConfig)
       .withMethod(config.method)
-      .withHttpHeaders("Content-Type" -> "application/x-www-form-urlencoded")
+      .withHttpHeaders("Content-Type" -> ctype)
       .withBody(body)
       .execute()
       .flatMap { resp =>
         val respBody = resp.body
         if (resp.status == 200) {
-          val token = resp.json.select("access_token").as[String]
-          val expires_in: Long = resp.json.select("expires_in").asOpt[Long].getOrElse(config.cacheTokenSeconds.toSeconds)
-          env.datastores.rawDataStore.set(key, ByteString(token), expires_in.seconds.toMillis.some).map { _ =>
-            Right(token)
+          if (resp.contentType.toLowerCase().equals("application/x-www-form-urlencoded")) {
+            val body = resp.body.split("&").map{ p =>
+              val parts = p.split("=")
+              (parts.head, parts.last)
+            }.toMap
+            val token = body.getOrElse("access_token", "--")
+            val expires_in: Long = body.getOrElse("expires_in", config.cacheTokenSeconds.toSeconds.toString).toLong
+            env.datastores.rawDataStore.set(key, ByteString(token), expires_in.seconds.toMillis.some).map { _ =>
+              Right(token)
+            }
+          } else {
+            val token = resp.json.select("access_token").as[String]
+            val expires_in: Long = resp.json.select("expires_in").asOpt[Long].getOrElse(config.cacheTokenSeconds.toSeconds)
+            env.datastores.rawDataStore.set(key, ByteString(token), expires_in.seconds.toMillis.some).map { _ =>
+              Right(token)
+            }
           }
         } else {
           Left((respBody, resp.status)).future
@@ -117,9 +137,11 @@ class OAuth2Caller extends RequestTransformer {
       }
   }
 
+  def computeKey(env: Env, config: OAuth2CallerConfig, descriptor: ServiceDescriptor): String = s"${env.storageRoot}:plugins:oauth-caller-plugin:${config.kind.name}:${config.url}:${config.clientId}:${config.user.getOrElse("--")}:${config.password.getOrElse("--")}:${descriptor.id}"
+
   override def transformRequestWithCtx(ctx: TransformerRequestContext)(implicit env: Env, ec: ExecutionContext, mat: Materializer): Future[Either[Result, HttpRequest]] = {
     val config = OAuth2CallerConfig.parse(ctx.configFor(configRoot.get))
-    val key = s"${env.storageRoot}:plugins:oauth-caller-plugin:${config.kind.name}:${config.url}:${config.clientId}:${config.user.getOrElse("--")}:${config.password.getOrElse("--")}:${ctx.descriptor.id}"
+    val key = computeKey(env, config, ctx.descriptor)
     env.datastores.rawDataStore.get(key).flatMap {
       case Some(token) => Right(ctx.otoroshiRequest.copy(headers = ctx.otoroshiRequest.headers + (config.headerName -> config.headerValueFormat.format(token.utf8String)))).future
       case None => getToken(key, config).map {
@@ -131,7 +153,7 @@ class OAuth2Caller extends RequestTransformer {
 
   override def transformResponseWithCtx(ctx: TransformerResponseContext)(implicit env: Env, ec: ExecutionContext, mat: Materializer): Future[Either[Result, HttpResponse]] = {
     val config = OAuth2CallerConfig.parse(ctx.configFor(configRoot.get))
-    val key = s"${env.storageRoot}:plugins:oauth-caller-plugin:${config.kind.name}:${config.url}:${config.clientId}:${config.user.getOrElse("--")}:${config.password.getOrElse("--")}:${ctx.descriptor.id}"
+    val key = computeKey(env, config, ctx.descriptor)
     if (ctx.otoroshiResponse.status == 401) {
       getToken(key, config).map {
         case Left((body, status)) => Left(Results.Unauthorized(Json.obj("error" -> "unauthorized", "error_description" -> body, "error_status" -> status)))
