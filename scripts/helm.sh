00c441c50c08a7f3bfed82390a3c993a7cf570b0
Etienne ANNE <etienne.anne@icloud.com>
2021-11-17 11:28:52 +0100

    fix generation of helm bundle

--- scripts/helm.sh
+++ scripts/helm.sh
@@ -1,6 +1,6 @@
 HERE=`pwd`
 
-helm package ./kubernetes/helm/otoroshi
-mkdir ./docs/helm
+helm package --dependency-update ./kubernetes/helm/otoroshi
+mkdir -p ./docs/helm
 mv otoroshi-1.0.0.tgz ./docs/helm/otoroshi-1.0.0.tgz
 helm repo index ./docs/helm --url https://maif.github.io/otoroshi/helm
