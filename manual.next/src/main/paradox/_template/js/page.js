f83848c844084894a67550ff81e7ec47cac91531
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2021-12-08 15:34:54 +0100

    Fix search

--- manual.next/src/main/paradox/_template/js/page.js
+++ manual.next/src/main/paradox/_template/js/page.js
@@ -46,12 +46,12 @@ $(function () {
         var search = index.search(searched, { expand: true });
         var foundDocs = search.map(f => {
           return data.filter(d => d.url === f.ref)[0];
-        });
+        }).filter(d => d.id !== '/entities/index.md' && d.id !== '/includes/initialize.md');
         const rect = e.target.getBoundingClientRect();
         $('#search-results').css('left', rect.left).css('top', rect.top + rect.height).css('width', rect.width);
-        console.log('\n\n----------------------------------------------------')
-        console.log(foundDocs.slice(0, 10).map(p => ({ ...p, content: null })))
-        console.log('----------------------------------------------------\n\n')
+        // console.log('\n\n----------------------------------------------------')
+        // console.log(foundDocs.slice(0, 10).map(p => ({ title: p.title, id: p.id })))
+        // console.log('----------------------------------------------------\n\n')
         var foundDocsHtml = foundDocs.length > 0 ? foundDocs.slice(0, 10).map(d => {
           return '<a style="height: 50px; background-color: #fbfbfb; padding: 10px;" href="' + additionalPath + d.url + '">' + d.title + '</a>';
         }).join('') : '<span style="height: 50px; background-color: #fbfbfb; padding: 10px;>No results</span>';
