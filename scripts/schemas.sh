b05a7d3a2aabbaa80b2161798347019474db56d2
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2021-12-14 09:58:53 +0100

    Fix schema generation

--- scripts/schemas.sh
+++ scripts/schemas.sh
@@ -5,7 +5,7 @@ LOCATION=`pwd`
 FILES="$LOCATION/manual/src/main/paradox/schemas/*.ditaa"
 for f in $FILES
 do
-  FILE=`echo $f | sed 's/ditaa/png/g' | sed 's/schemas/img/g'`
+  FILE=`echo $f | sed 's/ditaa/png/g' | sed 's/schemas/imgs/g'`
   echo "java -jar $LOCATION/scripts/tools/ditaa.jar -E -S -o $f $FILE"
 	java -jar $LOCATION/scripts/tools/ditaa.jar -E -S -o $f $FILE
 done
