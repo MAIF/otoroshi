2e5bc4929900a935cbf9bb76c1c15bba5b262d1d
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2021-12-15 23:02:15 +0100

    remove manual.next

--- docs/manual.next/snippets/fetch.sh
+++
@@ -1,7 +0,0 @@
-// #curl
-curl -L -o otoroshi.jar 'https://github.com/MAIF/otoroshi/releases/download/v1.5.0-dev/otoroshi.jar'
-// #curl
-
-// #wget
-wget 'https://github.com/MAIF/otoroshi/releases/download/v1.5.0-dev/otoroshi.jar'
-// #wget
