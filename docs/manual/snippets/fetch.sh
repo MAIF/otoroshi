6da93afb4d23d0de0d820fd618520574131fcf15
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2021-12-17 21:34:17 +0100

    Update site documentation before release

---
+++ docs/manual/snippets/fetch.sh
@@ -0,0 +1,7 @@
+// #curl
+curl -L -o otoroshi.jar 'https://github.com/MAIF/otoroshi/releases/download/v1.5.0/otoroshi.jar'
+// #curl
+
+// #wget
+wget 'https://github.com/MAIF/otoroshi/releases/download/v1.5.0/otoroshi.jar'
+// #wget
