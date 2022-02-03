8aa15202e3c47af240f2ec8ddd58110658a6fcbb
Baudelot Philippe <philippe@baudelot.eu>
2022-02-03 11:16:44 +0100

    new skin for separator

--- otoroshi/javascript/src/components/Separator.js
+++ otoroshi/javascript/src/components/Separator.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export class Separator extends Component {
   render() {
     return (
-      <div className="mb-3">
-        <label className="control-label col-sm-2" />
-        <div className="col-sm-10" style={{ borderBottom: '1px solid #666', paddingBottom: 5 }}>
+      <div className="row mb-3">
+        <label className="col-sm-2" />
+        <div className="col-sm-12 pb-2" style={{ borderBottom: '1px solid #666'}}>
           {this.props.title}
         </div>
       </div>
