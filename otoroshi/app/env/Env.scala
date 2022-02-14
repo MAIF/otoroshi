35e0a7fc7c9af82951c919bbae9d57433c80aeea
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2022-02-14 13:29:31 +0100

    Update version to 1.5.0-dev

--- otoroshi/app/env/Env.scala
+++ otoroshi/app/env/Env.scala
@@ -976,7 +976,7 @@ class Env(
     useAkkaHttpClient = true
   )
 
-  lazy val otoroshiVersion    = "1.5.3"
+  lazy val otoroshiVersion    = "1.5.0-dev"
   lazy val otoroshiVersionSem = Version(otoroshiVersion)
   lazy val checkForUpdates    = configuration.getOptionalWithFileSupport[Boolean]("app.checkForUpdates").getOrElse(true)
 
