825a5e8fb958fb04ef24944d6c5d784ad76c48dd
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2022-02-14 10:21:56 +0100

    Fix mtls test

--- scripts/tools/mtls-tests/certs.sh
+++ scripts/tools/mtls-tests/certs.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -out cert-backend.pem -keyout cert-backend-key.pem -subj "/CN=localhost"
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -out cert-frontend.pem -keyout cert-frontend-key.pem -subj "/CN=mtls.oto.tools"
+delete ./cert-backend.pem ./cert-backend-key.pem
+delete ./cert-frontend.pem ./cert-frontend-key.pem
+
+#openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -out cert-backend.pem -keyout cert-backend-key.pem -subj "/CN=localhost" -addext "subjectAltName = DNS:localhost"
+#openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -out cert-frontend.pem -keyout cert-frontend-key.pem -subj "/CN=mtls.oto.tools" -addext "subjectAltName = DNS:mtls.oto.tools"
+
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -out cert-backend.pem -keyout cert-backend-key.pem -extensions v3_ca -config ./backend.cfg
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -out cert-frontend.pem -keyout cert-frontend-key.pem -extensions v3_ca -config ./frontend.cfg
