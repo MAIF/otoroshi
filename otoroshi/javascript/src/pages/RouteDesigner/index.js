398603daa61bc61585bccebe5a277c32cf96ac9d
Etienne ANNE <etienne.anne@icloud.com>
2022-05-11 12:29:40 +0200

    wip designer

--- otoroshi/javascript/src/pages/RouteDesigner/index.js
+++ otoroshi/javascript/src/pages/RouteDesigner/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, Route, Switch, useLocation, useParams, useRouteMatch } from 'react-router-dom';
+import { Route, Switch, useHistory, useLocation, useParams, useRouteMatch } from 'react-router-dom';
 import { nextClient } from '../../services/BackOfficeServices';
 import Designer from './Designer';
 import { TryIt } from './TryIt';
@@ -14,7 +14,84 @@ import { ServiceHealthPage } from '../ServiceHealthPage';
 import { ServiceAnalyticsPage } from '../ServiceAnalyticsPage';
 import { ServiceApiKeysPage } from '../ServiceApiKeysPage';
 import { useEntityFromURI } from '../../util';
-import { FeedbackButton } from './FeedbackButton';
+
+const Manager = ({ query, entity, ...props }) => {
+    const p = useParams();
+    const isCreation = p.routeId === 'new';
+    const history = useHistory()
+
+    const [value, setValue] = useState();
+    const [saveButton, setSaveButton] = useState(null)
+
+    useEffect(() => {
+        if (p.routeId === 'new') {
+            nextClient.template(nextClient.ENTITIES[entity.fetchName]).then(setValue);
+        } else
+            nextClient.fetch(nextClient.ENTITIES[entity.fetchName], p.routeId).then(setValue);
+    }, [p.routeId]);
+
+    useEffect(() => {
+        if (value && value.id) {
+            props.setSidebarContent(<DesignerSidebar route={value} />);
+
+            props.setTitle(() => <div className='page-header d-flex align-item-center justify-content-between ms-0 mb-3'>
+                <h4 className='flex' style={{ margin: 0 }}>
+                    {{
+                        flow: 'Designer',
+                        'try-it': 'Test routes',
+                        informations: 'Informations',
+                        routes: 'Routes'
+                    }[query]}
+                </h4>
+                <div className='d-flex align-item-center justify-content-between flex'>
+                    {[
+                        { to: `/${entity.link}/${value.id}?tab=informations`, icon: 'fa-file-alt', title: 'Informations', tab: 'informations' },
+                        { to: `/${entity.link}/${value.id}?tab=routes`, icon: 'fa-road', title: 'Routes', tab: 'routes', enabled: ['route-compositions'] },
+                        { to: `/${entity.link}/${value.id}?tab=flow`, icon: 'fa-pencil-ruler', title: 'Designer', tab: 'flow' },
+                        { to: `/${entity.link}/${value.id}?tab=try-it`, icon: 'fa-vials', title: 'Tester', tab: 'try-it' },
+                    ]
+                        .filter(link => !link.enabled || link.enabled.includes(entity))
+                        .map(({ to, icon, title, tooltip, tab }) => (
+                            <button
+                                onClick={() => {
+                                    if (query !== tab)
+                                        history.push(to)
+                                }}
+                                {...(tooltip || {})}
+                                className="btn btn-sm toggle-form-buttons ms-2"
+                                key={title}
+                                style={{
+                                    backgroundColor: tab === query ? '#f9b000' : '#494948',
+                                    color: '#fff'
+                                }}>
+                                <i className={`fas ${icon}`} /> {title}
+                            </button>
+                        ))}
+                    {saveButton}
+                </div>
+            </div>)
+        }
+    }, [value, saveButton]);
+
+    const divs = [
+        { predicate: query && query === 'flow' && !isCreation, render: () => <Designer {...props} value={value} setSaveButton={setSaveButton} /> },
+        { predicate: query && query === 'try-it', render: () => <TryIt route={value} /> },
+        { predicate: query && query === 'routes', render: () => value && <RouteCompositions service={value} /> }
+    ]
+
+    const component = divs.filter(p => p.predicate)
+
+    if (component.length > 0)
+        return <div className="designer row">
+            {component[0].render()}
+        </div>
+
+    return (
+        <div className="designer row ps-3">
+            <Informations {...props} isCreation={isCreation} value={value} setValue={setValue} />
+        </div>
+    );
+}
 
 export default (props) => {
     const match = useRouteMatch();
@@ -70,77 +147,7 @@ export default (props) => {
             <Route
                 exact
                 path={`${match.url}/:routeId`}
-                component={() => {
-                    const p = useParams();
-                    const isCreation = p.routeId === 'new';
-                    const [value, setValue] = useState();
-                    const [saveButton, setSaveButton] = useState(null)
-
-                    useEffect(() => {
-                        if (p.routeId === 'new') {
-                            nextClient.template(nextClient.ENTITIES[entity.fetchName]).then(setValue);
-                        } else
-                            nextClient.fetch(nextClient.ENTITIES[entity.fetchName], p.routeId).then(setValue);
-                    }, [p.routeId]);
-
-                    useEffect(() => {
-                        if (value && value.id) {
-                            props.setSidebarContent(<DesignerSidebar route={value} />);
-
-                            props.setTitle(() => <div className='page-header d-flex align-item-center justify-content-between ms-0 mb-3'>
-                                <h4 className='flex' style={{ margin: 0 }}>
-                                    {{
-                                        flow: 'Designer',
-                                        'try-it': 'Test routes',
-                                        informations: 'Informations',
-                                        routes: 'Routes'
-                                    }[query]}
-                                </h4>
-                                <div className='d-flex align-item-center justify-content-between flex'>
-                                    {[
-                                        { to: `/${entity.link}/${value.id}?tab=informations`, icon: 'fa-file-alt', title: 'Informations', tab: 'informations' },
-                                        { to: `/${entity.link}/${value.id}?tab=routes`, icon: 'fa-road', title: 'Routes', tab: 'routes', enabled: ['route-compositions'] },
-                                        { to: `/${entity.link}/${value.id}?tab=flow`, icon: 'fa-pencil-ruler', title: 'Designer', tab: 'flow' },
-                                        { to: `/${entity.link}/${value.id}?tab=try-it`, icon: 'fa-vials', title: 'Tester', tab: 'try-it' },
-                                    ]
-                                        .filter(link => !link.enabled || link.enabled.includes(entity))
-                                        .map(({ to, icon, title, tooltip, tab }) => (
-                                            <Link
-                                                to={to}
-                                                {...(tooltip || {})}
-                                                className="btn btn-sm toggle-form-buttons ms-2" key={title}
-                                                style={{
-                                                    backgroundColor: tab === query ? '#f9b000' : '#494948',
-                                                    color: '#fff'
-                                                }}>
-                                                <i className={`fas ${icon}`} /> {title}
-                                            </Link>
-                                        ))}
-                                    {saveButton}
-                                </div>
-                            </div>)
-                        }
-                    }, [value, saveButton]);
-
-                    const divs = [
-                        { predicate: query && query === 'flow' && !isCreation, render: () => <Designer {...props} value={value} setSaveButton={setSaveButton} /> },
-                        { predicate: query && query === 'try-it', render: () => <TryIt route={value} /> },
-                        { predicate: query && query === 'routes', render: () => value && <RouteCompositions service={value} /> }
-                    ]
-
-                    const component = divs.filter(p => p.predicate)
-
-                    if (component.length > 0)
-                        return <div className="designer row">
-                            {component[0].render()}
-                        </div>
-
-                    return (
-                        <div className="designer row ps-3">
-                            <Informations {...props} isCreation={isCreation} value={value} setValue={setValue} />
-                        </div>
-                    );
-                }}
+                children={() => <Manager query={query} {...props} entity={entity} />}
             />
             <Route component={Routes} />
         </Switch>
