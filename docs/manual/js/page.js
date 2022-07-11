06d3c1e6377542eb3478cf23206f5efcab0da9d1
Etienne ANNE <etienne.anne@icloud.com>
2022-07-11 15:02:19 +0200

    fix js

--- docs/manual/js/page.js
+++ docs/manual/js/page.js
@@ -64,25 +64,27 @@ $(function () {
   setupSearch();
 
   function improveSidebar() {
-    let list = document.getElementsByClassName("active")[1]
-      .parentElement
-      .getElementsByTagName("ul")[0]
-
-    if (!list) {
-      list = document.getElementsByClassName("active")[1]
-        .parentElement
-        .parentElement
-        .parentElement
-        .children[0]
+    if (document.getElementsByClassName("active")[1]) {
+      let list = document.getElementsByClassName("active")[1]
         .parentElement
         .getElementsByTagName("ul")[0]
-    }
 
-    if (list && list.children)
-      for (let i = 0; i < list.children.length; i++) {
-        let r = list.children[i]
-        r.style.display = "block"
+      if (!list) {
+        list = document.getElementsByClassName("active")[1]
+          .parentElement
+          .parentElement
+          .parentElement
+          .children[0]
+          .parentElement
+          .getElementsByTagName("ul")[0]
       }
+
+      if (list && list.children)
+        for (let i = 0; i < list.children.length; i++) {
+          let r = list.children[i]
+          r.style.display = "block"
+        }
+    }
   }
 
   improveSidebar();
