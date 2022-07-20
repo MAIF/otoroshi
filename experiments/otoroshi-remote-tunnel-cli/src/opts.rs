1acacc41308d19a40ffbde9c838b1acb14385841
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2022-07-20 16:18:27 +0200

    split code

---
+++ experiments/otoroshi-remote-tunnel-cli/src/opts.rs
@@ -0,0 +1,49 @@
+use clap::{Parser};
+
+#[derive(Parser, Clone, Debug)]
+#[clap(name = "otoroshi_remote_tunnel_cli")]
+#[clap(version = "0.1.0")]
+#[clap(about = "Route local process to a remote Otoroshi through a tunnel (experimental)", long_about = None)]
+pub struct Opts {
+    /// the otoroshi api hostname
+    #[clap(long, default_value = "otoroshi-api.oto.tools:9999")]
+    pub host: String,
+    /// the otoroshi client-id
+    #[clap(long, default_value = "admin-api-apikey-id")]
+    pub cid: String,
+    /// the otoroshi client-secret
+    #[clap(long, default_value = "admin-api-apikey-secret")]
+    pub csec: String,
+    /// the otoroshi client-secret
+    #[clap(long, default_value = "cli")]
+    pub tunnel: String,
+    /// enable tls want mode
+    #[clap(long, action, default_value = "false")]
+    pub tls: bool,
+
+    /// the local host forwarded to
+    #[clap(long, default_value = "localhost")]
+    pub local_host: String,
+    /// the local port forwarded to
+    #[clap(long, default_value = "8080")]
+    pub local_port: i32,
+    /// local process exposed as tls ?
+    #[clap(long, action, default_value = "false")]
+    pub local_tls: bool,
+
+    /// enable expose mode
+    #[clap(long, action, default_value = "false")]
+    pub expose: bool,
+    /// the exposed domain
+    #[clap(long)]
+    pub remote_domain: Option<String>,
+    /// the exposed subdomain
+    #[clap(long)]
+    pub remote_subdomain: Option<String>,
+}
+
+impl Opts {
+  pub fn build_from_command_line() -> Opts {
+    Opts::parse()
+  }
+}
---
+++ experiments/otoroshi-remote-tunnel-cli/src/tunnel.rs
@@ -0,0 +1,110 @@
+use std::collections::HashMap;
+use hyper::http::request::Builder;
+use futures_util::{SinkExt, StreamExt};
+use tokio_tungstenite::{
+    connect_async,
+    tungstenite::{Result, Message},
+};
+use url::Url;
+use base64::{encode, decode};
+use hyper::{Request, Client};
+use clap::{Parser};
+use std::time::Instant;
+
+use crate::opts::Opts;
+
+pub struct Tunnel {}
+
+impl Tunnel {
+    pub async fn connect(opts: Opts) -> Result<()> {
+      let tunnel_id = opts.tunnel;
+      let client_id = opts.cid;
+      let client_secret = opts.csec;
+      let scheme = if opts.tls {
+          "wss"
+      } else {
+          "ws"
+      };
+      let host = opts.host;
+
+      let client = Client::new();
+      let credentials = encode(format!("{}:{}", client_id, client_secret));
+      let url_raw = format!("{}://{}/api/tunnels/register?tunnel_id={}&basic_auth={}&pong_ping=true", scheme, host, tunnel_id, credentials);
+      // debug!("url_raw {} {}", opts.tls, url_raw);
+      let url =  Url::parse(url_raw.as_str()).expect("Bad URL");
+      // TODO: connect tls
+      let (ws_stream, _) = Box::leak(Box::new(connect_async(url).await?));
+
+      info!("connection done !");
+      info!("");
+      
+      while let Some(msg) = ws_stream.next().await {
+          let msg = msg?;
+          if msg.is_binary() {
+              let json: serde_json::Value = serde_json::from_slice(&msg.into_data()).unwrap();
+              let msg_type = json.get("type").unwrap().as_str().unwrap();
+              // info!("{}", msg_type);
+              if msg_type == "request" {
+                  // TODO: cookies
+                  let start = Instant::now();
+                  let request_id = json.get("request_id").unwrap().as_i64().unwrap();
+                  let addr = json.get("addr").unwrap().as_str().unwrap();
+                  let version = json.get("version").unwrap().as_str().unwrap();
+                  let path = json.get("path").unwrap().as_str().unwrap();
+                  let method = json.get("method").unwrap().as_str().unwrap();
+                  let uri = json.get("url").unwrap().as_str().unwrap();
+                  let headers = json.get("headers").unwrap().as_object().unwrap();
+                  // debug!("headers: {:?}", headers);
+                  let mut builder: Builder = Request::builder()
+                      .method(method)
+                      .uri(uri);
+                  for header in headers.iter() {
+                      builder = builder.header(header.0, header.1.as_str().unwrap());
+                  }
+                  let maybe_body = json.get("body");
+                  let req: Request<hyper::Body> = if maybe_body.is_some() {
+                      let body_bytes = decode(maybe_body.unwrap().as_str().unwrap()).unwrap();
+                      builder.body(hyper::Body::from(body_bytes)).unwrap()
+                  } else {
+                      builder.body(hyper::Body::empty()).unwrap()
+                  };
+                  let resp = client.request(req).await.unwrap();
+                  let status = resp.status();
+                  let resp_headers = resp.headers().clone();
+                  let mut resp_headers_map = HashMap::new();
+                  for (key, value) in resp_headers.iter() {
+                      resp_headers_map.insert(key.as_str(), value.to_str().unwrap());
+                  }
+                  let body_bytes = hyper::body::to_bytes(resp).await.unwrap();
+                  let body_str = encode(body_bytes);
+                  let resp_json = serde_json::json!({
+                      "status": status.as_u16(),
+                      "headers": resp_headers_map,
+                      "body": body_str,
+                      "request_id": request_id,
+                      "type": "response",
+                  });
+                  let resp_json_bin = serde_json::to_vec(&resp_json).unwrap();
+                  let elasped = start.elapsed();
+                  info!("{} - - [{:?}] \"{} {} {}\" {} {}", addr, chrono::offset::Utc::now(), method, path, version, status.as_u16(), elasped.as_millis());
+                  match ws_stream.send(Message::Binary(resp_json_bin)).await {
+                      Ok(_) => (),
+                      Err(e) => debug!("error while sending response: {}", e)
+                  };
+              } else if msg_type == "pong" {
+                  let json = serde_json::json!({
+                      "tunnel_id": tunnel_id.clone(), 
+                      "type": "ping"
+                  });
+                  let bytes = serde_json::to_vec(&json).unwrap();
+                  match ws_stream.send(Message::Binary(bytes)).await {
+                      Ok(_) => (),
+                      Err(e) => debug!("error while sending pong: {}", e)
+                  };
+              }
+          }
+      }
+      Ok(())
+  }
+}
+
