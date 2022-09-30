1267bd8a59ba59753dac92f480e38b1f474a0849
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2022-09-30 16:21:49 +0200

    Update version to 1.5.14

--- otoroshi/app/openapi/openapi.scala
+++ otoroshi/app/openapi/openapi.scala
@@ -1190,7 +1190,7 @@ class OpenApiGenerator(
       "info"         -> Json.obj(
         "title"       -> "Otoroshi Admin API",
         "description" -> "Admin API of the Otoroshi reverse proxy",
-        "version"     -> "1.5.0-dev",
+        "version"     -> "1.5.14",
         "contact"     -> Json.obj(
           "name"  -> "Otoroshi Team",
           "email" -> "oss@maif.fr"
