b21a06e8525dce131d4cb87eb55a3a3258e5bddf
baudelotphilippe <philippe@baudelot.eu>
2022-11-24 12:30:38 +0100

    add content

--- otoroshi/javascript/src/pages/AtomicDesignPage.js
+++ otoroshi/javascript/src/pages/AtomicDesignPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Button } from "../components/Button";
 import { SimpleBooleanInput } from "../components/inputs";
+import { PillButton } from "../components/PillButton";
 
 export class AtomicDesignPage extends Component {
   render() {
@@ -22,12 +23,27 @@ export class AtomicDesignPage extends Component {
         <Button type="danger" className="btn-sm" text="danger sm" style={{margin:10}} />
         <Button type="danger" text="danger" style={{margin:10}}/>
         <Button type="danger" text="danger & disabled" disabled="true" style={{margin:10}}/>
-
         <br />
         <Button type="success" className="btn-sm" text="success" style={{margin:10}}/>
         <Button type="success" text="success" style={{margin:10}}/>
         <Button type="success" text="success & disabled" disabled="true" style={{margin:10}}/>
         <br />
+        <Button type="save" className="btn-sm" text="save" style={{margin:10}}/>
+        <Button type="save" text="save" style={{margin:10}}/>
+        <Button type="save" text="save & disabled" disabled="true" style={{margin:10}}/>
+        <br />
+        <Button type="default" className="btn-sm" text="default" style={{margin:10}}/>
+        <Button type="default" text="default" style={{margin:10}}/>
+        <Button type="default" text="default & disabled" disabled="true" style={{margin:10}}/>
+        <br />
+        <p style={{color:"var(--color-primary)"}}>Pill button</p>
+        <PillButton
+        rightEnabled=""
+        onChange=""
+        leftText="Design"
+        rightText="Content"
+      />
+      <p style={{color:"var(--color-primary)"}}>Boolean</p>
         <SimpleBooleanInput value="disabled" onChange={(e) => e} />
         <hr />
         <p style={{color:"var(--color-primary)"}}>Badges</p>
@@ -38,17 +54,20 @@ export class AtomicDesignPage extends Component {
         <span style={{ backgroundColor: "var(--color-green)", }} >  --color-green </span>
         <span style={{ backgroundColor: "var(--color-blue)", }} > --color-blue </span>
         <span style={{ backgroundColor: "var(--color-red)", }} > --color-red </span>
-
-        <p style={{color:"var(--color-primary)"}}>Colors Dark Mode</p>
         <span style={{ backgroundColor: "var(--color-primary)", }} > --color-primary </span>
         <span style={{ backgroundColor: "var(--color-primary-lighter)", }} > --color-primary-lighter </span>
 
-        <span style={{ backgroundColor: "var(--bg-color_primary)", }} > --bg-color_primary </span>
-        <span style={{ backgroundColor: "var(--bg-color_1)", }} > --bg-color_1 </span>
-        <span style={{ backgroundColor: "var(--bg-color_2)", }} > --bg-color_2 </span>
-        <span style={{ backgroundColor: "var(--color_2)", }} > --color_2 </span>
-        <p style={{color:"var(--color-primary)"}}>Colors Light Mode</p>
+        <p style={{color:"var(--color-primary)"}}>Colors & bgcolors levels for Dark/White Mode</p>
+        <div style={{width:400, height:400, backgroundColor: "var(--bg-color_level1)", color:"var(--color_level1)"}}>
+          Level 1
+        <div style={{width:200, height:200, margin:'0 auto',backgroundColor: "var(--bg-color_level2)", color:"var(--color_level2)"}}>
+          Level 2
+</div>
 
+        </div>
+        <span style={{ backgroundColor: "var(--bg-color_level1)", color:"var(--color_level1)"}} > --bg-color_level1 & --color_level1 </span>
+        <span style={{ backgroundColor: "var(--bg-color_level2)", color:"var(--color_level2)"}} > --bg-color_level2 & --color_level2 </span>
+        
       </div>
     );
   }
