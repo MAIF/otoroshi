6818c002c27d7650fa66ac77f278ec38a99be1cd
baudelotphilippe <philippe@baudelot.eu>
2022-11-29 18:59:23 +0100

    fix add class in Button

--- otoroshi/javascript/src/components/SquareButton.js
+++ otoroshi/javascript/src/components/SquareButton.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Button } from './Button';
 
 export function SquareButton({ text, icon, level = 'info', ...props }) {
+  const { className = "", ...restProps } = props; 
   return (
     <Button
       type={level}
-      className="btn-sm d-flex flex-column align-items-center justify-content-center square-button"
+      className={`btn-sm d-flex flex-column align-items-center justify-content-center square-button ${className}`}
       style={{
-        ...(props.style || {}),
+        ...(restProps.style || {}),
       }}
-      {...props}>
+      {...restProps}>
       <div>
         <i className={`mb-2 fas ${icon}`} />
       </div>
