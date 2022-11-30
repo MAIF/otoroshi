28133be6ee5db7ae5e6ffbf90d959b138397bba6
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2022-11-30 15:46:00 +0100

    Update openapi file before release

--- docs/manual/snippets/build.sbt
+++
@@ -1,2 +0,0 @@
-libraryDependencies += "fr.maif" %% "otoroshi" % "1.5.19"
-libraryDependencies += "fr.maif" %% "otoroshi" % "1.5.19" classifier "assets"
--- docs/manual/snippets/fetch.sh
+++
@@ -1,7 +0,0 @@
-// #curl
-curl -L -o otoroshi.jar 'https://github.com/MAIF/otoroshi/releases/download/v1.5.19/otoroshi.jar'
-// #curl
-
-// #wget
-wget 'https://github.com/MAIF/otoroshi/releases/download/v1.5.19/otoroshi.jar'
-// #wget
