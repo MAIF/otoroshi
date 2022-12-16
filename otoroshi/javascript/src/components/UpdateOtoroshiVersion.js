1126933cb6be5113d876555237c5c1e96c5fce96
Etienne ANNE <etienne.anne@icloud.com>
2022-12-16 14:51:06 +0100

    fix new version modal

--- otoroshi/javascript/src/components/UpdateOtoroshiVersion.js
+++ otoroshi/javascript/src/components/UpdateOtoroshiVersion.js
@@ -15,34 +15,36 @@ export class UpdateOtoroshiVersion extends Component {
     if (this.props.env && this.props.env.version && this.props.env.version.outdated && show) {
       return (
         <div className="topbar-popup">
-          <button
-            type="button"
-            className="btn btn-sm"
-            style={{ alignSelf: 'flex-end' }}
-            onClick={(e) => {
-              e.preventDefault();
-              localStorage.setItem(
-                'otoroshi_outdated_popup',
-                JSON.stringify({ closed_at: Date.now() })
-              );
-              this.forceUpdate();
-            }}>
-            <i className="fas fa-times" />
-          </button>
-          <a
-            style={{ alignSelf: 'center' }}
-            target="_blank"
-            onClick={(e) => {
-              localStorage.setItem(
-                'otoroshi_outdated_popup',
-                JSON.stringify({ closed_at: Date.now() })
-              );
-              this.forceUpdate();
-            }}
-            href={`https://github.com/MAIF/otoroshi/releases/tag/${this.props.env.version.version_raw}`}>
-            <i className="fab fa-github" style={{ color: '#856404', marginRight: 5 }}></i>A new
-            version of Otoroshi is available ({this.props.env.version.version_raw})
-          </a>
+          <div style={{ position: 'relative' }} className="d-flex align-items-center">
+            <button
+              type="button"
+              className="btn btn-sm"
+              style={{ alignSelf: 'flex-end', position: 'absolute', right: -18, top: 4 }}
+              onClick={(e) => {
+                e.preventDefault();
+                localStorage.setItem(
+                  'otoroshi_outdated_popup',
+                  JSON.stringify({ closed_at: Date.now() })
+                );
+                this.forceUpdate();
+              }}>
+              <i className="fas fa-times" />
+            </button>
+            <a
+              style={{ alignSelf: 'center' }}
+              target="_blank"
+              onClick={(e) => {
+                localStorage.setItem(
+                  'otoroshi_outdated_popup',
+                  JSON.stringify({ closed_at: Date.now() })
+                );
+                this.forceUpdate();
+              }}
+              href={`https://github.com/MAIF/otoroshi/releases/tag/${this.props.env.version.version_raw}`}>
+              <i className="fab fa-github" style={{ color: '#856404', marginRight: 5 }}></i>A new
+              version of Otoroshi is available ({this.props.env.version.version_raw})
+            </a>
+          </div>
         </div>
       );
     }
