8d2a22b27a48a2c44862e5282cb878ff085509a9
baudelotphilippe <philippe@baudelot.eu>
2023-01-03 15:56:44 +0100

    useless

--- otoroshi/javascript/src/components/PageTitle.js
+++ otoroshi/javascript/src/components/PageTitle.js
@@ -5,10 +5,10 @@ export default function PageTitle({ title, children, style = {} }) {
     <div
       className="page-header_title d-flex align-item-center justify-content-between ms-0 mb-3"
       style={style}>
-      <h3 className="flex" style={{ margin: 0, alignSelf: 'center' }}>
+      <h3 style={{ margin: 0, alignSelf: 'center' }}>
         {title}
       </h3>
-      <div className="d-flex align-item-center justify-content-between flex">{children}</div>
+      <div className="d-flex align-item-center justify-content-between">{children}</div>
     </div>
   );
 }
