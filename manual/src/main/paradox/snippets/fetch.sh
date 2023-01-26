74077de5b3141919400e61bf2d0416ede4d09e65
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2023-01-26 09:36:03 +0100

    [release 16.0.5] Update version to 16.0.0-dev

--- manual/src/main/paradox/snippets/fetch.sh
+++ manual/src/main/paradox/snippets/fetch.sh
@@ -1,7 +1,7 @@
 // #curl
-curl -L -o otoroshi.jar 'https://github.com/MAIF/otoroshi/releases/download/v16.0.5/otoroshi.jar'
+curl -L -o otoroshi.jar 'https://github.com/MAIF/otoroshi/releases/download/v16.0.0-dev/otoroshi.jar'
 // #curl
 
 // #wget
-wget 'https://github.com/MAIF/otoroshi/releases/download/v16.0.5/otoroshi.jar'
+wget 'https://github.com/MAIF/otoroshi/releases/download/v16.0.0-dev/otoroshi.jar'
 // #wget
