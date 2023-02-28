b44432548a8b4bf87a448ea025da53d8055c8367
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2023-02-28 15:51:57 +0100

    [release 16.1.0] Update site documentation before release

---
+++ docs/manual/snippets/build.sbt
@@ -0,0 +1,2 @@
+libraryDependencies += "fr.maif" %% "otoroshi" % "16.1.0"
+libraryDependencies += "fr.maif" %% "otoroshi" % "16.1.0" classifier "assets"
---
+++ docs/manual/snippets/embed.java
@@ -0,0 +1,62 @@
+package foo;
+
+import com.typesafe.config.ConfigFactory;
+import models.ServiceGroup;
+import play.api.Mode;
+import scala.None$;
+import scala.Option;
+import scala.Unit$;
+
+import java.io.File;
+import java.util.Arrays;
+import java.io.File;
+
+public class MyApp {
+
+    public static void main(String... args) {
+        otoroshi.api.Otoroshi oto = otoroshi.api.Otoroshi.apply(
+            play.core.server.ServerConfig.apply(
+                EmbedOto.class.getClassLoader(),
+                new File("."),
+                Option.<Object>apply(8888),
+                Option.<Object>apply(null),
+                "0.0.0.0",
+                Mode.Prod$.MODULE$,
+                System.getProperties()
+            ),
+            ConfigFactory.parseString(String.join("\n", Arrays.asList(
+                "app {",
+                "  storage = \"files\"",
+                "  importFrom = \"./my-state.json\"",
+                "  env = \"prod\"",
+                "  adminapi {",
+                "    targetSubdomain = \"otoroshi-admin-internal-api\"",
+                "    exposedSubdomain = \"otoroshi-api\"",
+                "    defaultValues {",
+                "      backOfficeGroupId = \"admin-api-group\"",
+                "      backOfficeApiKeyClientId = \"admin-api-apikey-id\"",
+                "      backOfficeApiKeyClientSecret = \"admin-api-apikey-secret\"",
+                "      backOfficeServiceId = \"admin-api-service\"",
+                "    }",
+                "  }",
+                "  claim {",
+                "    sharedKey = \"mysecret\"",
+                "  }",
+                "  filedb {",
+                "    path = \"./filedb/state.ndjson\"",
+                "  }",
+                "}"
+            )))
+        ).start();
+
+        ServiceGroup group = ServiceGroup.apply("id", "name", "description");
+
+        oto.dataStores().serviceGroupDataStore()
+          .set(group, None$.empty(), oto.executionContext(), oto.env())
+          .map(resp -> {
+            // Do whatever you want
+            oto.stop();
+            return Unit$.MODULE$;
+          }, oto.executionContext());
+    }
+}
