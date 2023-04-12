628f21cd88ab33e92de3ccc323e0bde9dea016d3
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2023-04-12 17:52:48 +0200

    fix #1547

--- otoroshi/app/next/plugins/users.scala
+++ otoroshi/app/next/plugins/users.scala
@@ -1,8 +1,12 @@
 package otoroshi.next.plugins
 
+import akka.Done
+import org.joda.time.DateTime
 import otoroshi.env.Env
 import otoroshi.gateway.Errors
-import otoroshi.next.plugins.api._
+import otoroshi.models.PrivateAppsUser
+import otoroshi.next.plugins.api.{NgPreRoutingError, _}
+import otoroshi.security.{IdGenerator, OtoroshiClaim}
 import otoroshi.utils.JsonPathUtils
 import otoroshi.utils.syntax.implicits._
 import play.api.Logger
@@ -107,10 +111,20 @@ class NgHasAllowedUsersValidator extends NgAccessValidator {
 }
 
 case class NgJwtUserExtractorConfig(
-
+  verifier: String,
+  strict: Boolean = true,
+  strip: Boolean = false,
+  namePath: Option[String] = None,
+  emailPath: Option[String] = None,
+  metaPath: Option[String] = None,
 ) extends NgPluginConfig {
   override def json: JsValue = Json.obj(
-
+    "verifier" -> verifier,
+    "strict" -> strict,
+    "strip" -> strip,
+    "name_path" -> namePath,
+    "email_path" -> emailPath,
+    "meta_path" -> metaPath,
   )
 }
 
@@ -119,7 +133,12 @@ object NgJwtUserExtractorConfig {
     override def writes(o: NgJwtUserExtractorConfig): JsValue = o.json
     override def reads(json: JsValue): JsResult[NgJwtUserExtractorConfig] = Try {
       NgJwtUserExtractorConfig(
-
+        verifier = json.select("verifier").as[String],
+        strict = json.select("strict").asOpt[Boolean].getOrElse(true),
+        strip = json.select("strip").asOpt[Boolean].getOrElse(false),
+        namePath = json.select("namePath").asOpt[String],
+        emailPath = json.select("emailPath").asOpt[String],
+        metaPath = json.select("metaPath").asOpt[String],
       )
     } match {
       case Failure(e) => JsError(e.getMessage)
@@ -128,20 +147,97 @@ object NgJwtUserExtractorConfig {
   }
 }
 
-class NgJwtUserExtractor extends NgFakePlugin {
+class NgJwtUserExtractor extends NgPreRouting {
 
-  override def name: String = Foo
-  override def description: Option[String] = Foo
-  override def defaultConfigObject: Option[NgPluginConfig] = NgJwtUserExtractorConfig().some
+  override def name: String = "Jwt user extractor"
+  override def description: Option[String] = "This plugin extract a user from a JWT token".some
+  override def defaultConfigObject: Option[NgPluginConfig] = NgJwtUserExtractorConfig("none").some
   override def multiInstance: Boolean = true
   override def core: Boolean = true
   override def visibility: NgPluginVisibility = NgPluginVisibility.NgUserLand
-  override def categories: Seq[NgPluginCategory] = Seq(NgPluginCategory.Foo)
-  override def steps: Seq[NgStep] = Seq(NgStep.Foo)
+  override def categories: Seq[NgPluginCategory] = Seq(NgPluginCategory.Authentication)
+  override def steps: Seq[NgStep] = Seq(NgStep.PreRoute)
 
-  override def plugin(ctx: NgFakePluginContext)(implicit env: Env, ec: ExecutionContext): Future[Foo] = {
-    val config = ctx.cachedConfig(internalName)(NgJwtUserExtractorConfig.format).getOrElse(NgJwtUserExtractorConfig())
+  private val registeredClaims = Seq(
+    "iss",
+    "sub",
+    "aud",
+    "exp",
+    "nbf",
+    "iat",
+    "jti"
+  )
 
+  override def preRoute(ctx: NgPreRoutingContext)(implicit env: Env, ec: ExecutionContext): Future[Either[NgPreRoutingError, Done]] = {
+    val config = ctx.cachedConfig(internalName)(NgJwtUserExtractorConfig.format).getOrElse(NgJwtUserExtractorConfig("none"))
+    env.datastores.globalJwtVerifierDataStore.findById(config.verifier).flatMap {
+      case None if !config.strict =>
+        Done.rightf
+      case None if config.strict  =>
+        NgPreRoutingErrorWithResult(Results.Unauthorized(Json.obj("error" -> "unauthorized", "error_description" -> "You have to provide a valid user"))).leftf
+      case Some(verifier)  => {
+        verifier
+          .verify(
+            ctx.request,
+            ctx.route.legacy,
+            None,
+            None,
+            ctx.attrs.get(otoroshi.plugins.Keys.ElCtxKey).get,
+            ctx.attrs
+          ) { jwtInjection =>
+            jwtInjection.decodedToken match {
+              case None if !config.strict => Results.Unauthorized(Json.obj()).future
+              case None if config.strict  => Results.Ok(Json.obj()).future
+              case Some(token)     => {
+                val jsonToken                         = new String(OtoroshiClaim.decoder.decode(token.getPayload))
+                val parsedJsonToken                   = Json.parse(jsonToken).as[JsObject]
+                val strippedJsonToken                 = JsObject(parsedJsonToken.value.filter {
+                  case (key, _) if registeredClaims.contains(key) => false
+                  case _                                          => true
+                })
+                val tokenMap: Map[String, String]     = parsedJsonToken.value.collect {
+                  case (key, JsNumber(number)) => (key, number.toString())
+                  case (key, JsString(value))  => (key, value)
+                  case (key, JsBoolean(value)) => (key, value.toString)
+                }.toMap
+                val meta: Option[JsValue]             =
+                  config.metaPath.flatMap(path => Try(JsonPathUtils.getAt[JsObject](jsonToken, path)).toOption.flatten)
+                val user: PrivateAppsUser             = PrivateAppsUser(
+                  randomId = IdGenerator.uuid,
+                  name = JsonPathUtils.getAt[String](jsonToken, config.namePath.getOrElse("name")).getOrElse("--"),
+                  email = JsonPathUtils.getAt[String](jsonToken, config.emailPath.getOrElse("email")).getOrElse("--"),
+                  profile = if (config.strip) strippedJsonToken else parsedJsonToken,
+                  token = Json.obj("jwt" -> token.getToken, "payload" -> parsedJsonToken),
+                  realm = s"JwtUserExtractor@${ctx.route.id}",
+                  authConfigId = s"JwtUserExtractor@${ctx.route.id}",
+                  otoroshiData = meta,
+                  createdAt = DateTime.now(),
+                  expiredAt = DateTime.now().plusHours(1),
+                  lastRefresh = DateTime.now(),
+                  tags = Seq.empty,
+                  metadata = Map.empty,
+                  location = ctx.route.location
+                )
+                ctx.attrs.put(otoroshi.plugins.Keys.UserKey -> user)
+                val newElContext: Map[String, String] = ctx.attrs.get(otoroshi.plugins.Keys.ElCtxKey).get ++ tokenMap
+                ctx.attrs.put(otoroshi.plugins.Keys.ElCtxKey -> newElContext)
+                Results.Ok(Json.obj()).future
+              }
+            }
+          }
+          .recover { case e: Throwable =>
+            Results.Unauthorized(Json.obj())
+          }
+          .flatMap { result =>
+            result.header.status match {
+              case 200 =>
+                Done.rightf
+              case _   =>
+                NgPreRoutingErrorWithResult(Results.Unauthorized(Json.obj("error" -> "unauthorized", "error_description" -> "You have to provide a valid user"))).leftf
+            }
+          }
+      }
+    }
   }
 }
   
--- otoroshi/javascript/src/forms/ng_plugins/NgJwtUserExtractor.js
+++ otoroshi/javascript/src/forms/ng_plugins/NgJwtUserExtractor.js
@@ -1,9 +1,37 @@
 export default {
   id: 'cp:otoroshi.next.plugins.NgJwtUserExtractor',
   config_schema: {
-
+    verifier: {
+      type: 'string',
+      label: 'JWT verifier id',
+    },
+    strict: {
+      type: 'bool',
+      label: 'Strict validation',
+    },
+    strip: {
+      type: 'bool',
+      label: 'Stripped token',
+    },
+    name_path: {
+      type: 'string',
+      label: 'Path to extract username',
+    },
+    email_path: {
+      type: 'string',
+      label: 'Path to extract user email',
+    },
+    meta_path: {
+      type: 'string',
+      label: 'Path to extract user metadata',
+    },
   },
   config_flow: [
-
+    'verifier',
+    'strict',
+    'strip',
+    'name_path',
+    'email_path',
+    'meta_path',
   ],
 };
