f0e5462f8d01f1641ca8c7038e536eb35aefbb34
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2023-05-16 12:01:56 +0200

    add first work around proxy_wasm

--- otoroshi/app/wasm/proxywasm/functions.scala
+++ otoroshi/app/wasm/proxywasm/functions.scala
@@ -23,14 +23,14 @@ object ProxyWasmFunctions {
         "proxy_log",
         parameters(3),
         parameters(1),
-        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.ProxyLog(plugin, params(0).v.i32, params(1).v.i32, params(2).v.i32),
+        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.proxyLog(plugin, params(0).v.i32, params(1).v.i32, params(2).v.i32),
         Optional.empty[EnvUserData]()
       ),
       new HostFunction[EnvUserData](
         "proxy_get_buffer_bytes",
         parameters(5),
         parameters(1),
-        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.ProxyGetBuffer(plugin, getCurrentVmData(),
+        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.proxyGetBuffer(plugin, getCurrentVmData(),
         params(0).v.i32,
         params(1).v.i32,
         params(2).v.i32,
@@ -42,21 +42,21 @@ object ProxyWasmFunctions {
         "proxy_set_effective_context",
         parameters(1),
         parameters(1),
-        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.ProxySetEffectiveContext(plugin, params(0).v.i32),
+        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.proxySetEffectiveContext(plugin, params(0).v.i32),
         Optional.empty[EnvUserData]()
       ),
       new HostFunction[EnvUserData](
         "proxy_get_header_map_pairs",
         parameters(3),
         parameters(1),
-        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.ProxyGetHeaderMapPairs(plugin, getCurrentVmData(), params(0).v.i32, params(1).v.i32, params(2).v.i32),
+        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.proxyGetHeaderMapPairs(plugin, getCurrentVmData(), params(0).v.i32, params(1).v.i32, params(2).v.i32),
         Optional.empty[EnvUserData]()
       ),
       new HostFunction[EnvUserData](
         "proxy_set_buffer_bytes",
         parameters(5),
         parameters(1),
-        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.ProxySetBuffer(plugin, getCurrentVmData(),
+        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.proxySetBuffer(plugin, getCurrentVmData(),
         params(0).v.i32,
         params(1).v.i32,
         params(2).v.i32,
@@ -68,7 +68,7 @@ object ProxyWasmFunctions {
         "proxy_get_header_map_value",
         parameters(5),
         parameters(1),
-        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.ProxyGetHeaderMapValue(plugin, getCurrentVmData(),
+        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.proxyGetHeaderMapValue(plugin, getCurrentVmData(),
         params(0).v.i32,
         params(1).v.i32,
         params(2).v.i32,
@@ -80,35 +80,35 @@ object ProxyWasmFunctions {
         "proxy_get_property",
         parameters(4),
         parameters(1),
-        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.ProxyGetProperty(plugin, getCurrentVmData(), params(0).v.i32, params(1).v.i32, params(2).v.i32, params(3).v.i32),
+        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.proxyGetProperty(plugin, getCurrentVmData(), params(0).v.i32, params(1).v.i32, params(2).v.i32, params(3).v.i32),
         Optional.empty[EnvUserData]()
       ),
       new HostFunction[EnvUserData](
         "proxy_increment_metric",
         Seq(LibExtism.ExtismValType.I32, LibExtism.ExtismValType.I64).toArray,
         parameters(1),
-        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.ProxyIncrementMetricValue(plugin, getCurrentVmData(), params(0).v.i32, params(1).v.i64),
+        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.proxyIncrementMetricValue(plugin, getCurrentVmData(), params(0).v.i32, params(1).v.i64),
         Optional.empty[EnvUserData]()
       ),
       new HostFunction[EnvUserData](
         "proxy_define_metric",
         parameters(4),
         parameters(1),
-        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.ProxyDefineMetric(plugin, params(0).v.i32, params(1).v.i32, params(2).v.i32, params(3).v.i32),
+        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.proxyDefineMetric(plugin, params(0).v.i32, params(1).v.i32, params(2).v.i32, params(3).v.i32),
         Optional.empty[EnvUserData]()
       ),
       new HostFunction[EnvUserData](
         "proxy_set_tick_period_milliseconds",
         parameters(1),
         parameters(1),
-        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.ProxySetTickPeriodMilliseconds(getCurrentVmData(), params(0).v.i32),
+        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.proxySetTickPeriodMilliseconds(getCurrentVmData(), params(0).v.i32),
         Optional.empty[EnvUserData]()
       ),
       new HostFunction[EnvUserData](
         "proxy_replace_header_map_value",
         parameters(5),
         parameters(1),
-        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.ProxyReplaceHeaderMapValue(plugin, getCurrentVmData(),
+        (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) => state.proxyReplaceHeaderMapValue(plugin, getCurrentVmData(),
         params(0).v.i32,
         params(1).v.i32,
         params(2).v.i32,
@@ -121,7 +121,7 @@ object ProxyWasmFunctions {
         parameters(8),
         parameters(1),
         (plugin: ExtismCurrentPlugin, params: Array[LibExtism.ExtismVal], returns: Array[LibExtism.ExtismVal], data: Optional[EnvUserData]) =>
-          state.ProxySendHttpResponse(plugin,
+          state.proxySendHttpResponse(plugin,
             params(0).v.i32,
             params(1).v.i32,
             params(2).v.i32,
