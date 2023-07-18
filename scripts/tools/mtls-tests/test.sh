5265ad1c9dc3cfe9fccc9c4b23b78ed83c7c4c0d
Benjamin Cavy <benjamin.cavy@gmail.com>
2023-07-18 16:36:48 +0200

    fix: attempt to fix flakky mtls test

--- scripts/tools/mtls-tests/test.sh
+++ scripts/tools/mtls-tests/test.sh
@@ -8,15 +8,17 @@ GODEBUG=x509ignoreCN=0 go run backendmtls.go &
 sleep 10
 GODEBUG=x509ignoreCN=0 go run clientbackend.go > clientbackend.out
 cp ../../../otoroshi/target/scala-2.12/otoroshi.jar ./otoroshi.jar
-java -Dapp.domain=oto.tools -Dhttp.port=18080 -Dhttps.port=18443 -jar otoroshi.jar &
+java -Dapp.domain=oto.tools -Dhttp.port=18080 -Dhttps.port=18443 -Dotoroshi.next.state-sync-interval=1000 -jar otoroshi.jar &
 sleep 20
 yarn install
 node oto.js
-sleep 10
+sleep 20
 GODEBUG=x509ignoreCN=0 go run clientfrontend.go > clientfrontend.out
 sleep 5
 # curl -k -H "Host: mtls.oto.tools" https://mtls.oto.tools:18443/ --include
 node check.js
+cat clientfrontend.out
+cat clientbackend.out
 rc=$?; if [ $rc != 0 ]; 
 then 
   kill $(ps aux | grep 'backendmtls.go' | awk '{print $2}')
