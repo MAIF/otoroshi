ed7bde6e14b68d49e58ed578a05f2b33de7a3939
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2023-08-01 15:21:59 +0200

    [release 16.7.0] Update version to 16.7.0

--- otoroshi/app/env/Env.scala
+++ otoroshi/app/env/Env.scala
@@ -1157,7 +1157,7 @@ class Env(
     name = backofficeRoute.name
   )
 
-  lazy val otoroshiVersion    = "16.6.0-dev"
+  lazy val otoroshiVersion    = "16.7.0"
   lazy val otoroshiVersionSem = Version(otoroshiVersion)
   lazy val checkForUpdates    = configuration.getOptionalWithFileSupport[Boolean]("app.checkForUpdates").getOrElse(true)
 
--- otoroshi/build.sbt
+++ otoroshi/build.sbt
@@ -2,7 +2,7 @@ import xerial.sbt.Sonatype._
 
 name := """otoroshi"""
 organization := "fr.maif"
-version := "16.6.0-dev"
+version := "16.7.0"
 scalaVersion := scalaLangVersion
 
 inThisBuild(
