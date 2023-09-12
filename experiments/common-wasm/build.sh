e1d43bf5d1fa19e1987a20b7601dd3aced5b5b10
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2023-09-12 13:44:01 +0200

    add izanami copy

--- experiments/common-wasm/build.sh
+++ experiments/common-wasm/build.sh
@@ -3,5 +3,6 @@ rm ./target/scala-2.13/common-wasm_2.13-*.jar
 # sbt '+package'
 sbt '+assembly'
 rm ../../otoroshi/lib/common-wasm_2.12-*.jar
-rm ../../otoroshi/lib/common-wasm_2.13-*.jar
+rm ../../../izanami-v2/lib/common-wasm_2.13-*.jar
 cp ./target/scala-2.12/common-wasm_2.12-*.jar ../../otoroshi/lib/
+cp ./target/scala-2.12/common-wasm_2.12-*.jar ../../../izanami-v2/lib/
