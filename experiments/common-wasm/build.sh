483708436cd67dbc14f952602c4530c1040b8d97
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2023-09-12 16:26:20 +0200

    bad scala version

--- experiments/common-wasm/build.sh
+++ experiments/common-wasm/build.sh
@@ -5,4 +5,4 @@ sbt '+assembly'
 rm ../../otoroshi/lib/common-wasm_2.12-*.jar
 rm ../../../izanami-v2/lib/common-wasm_2.13-*.jar
 cp ./target/scala-2.12/common-wasm_2.12-*.jar ../../otoroshi/lib/
-cp ./target/scala-2.12/common-wasm_2.12-*.jar ../../../izanami-v2/lib/
+cp ./target/scala-2.13/common-wasm_2.13-*.jar ../../../izanami-v2/lib/
