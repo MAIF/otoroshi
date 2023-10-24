6eba1af839d0715986da22074384ada34093223c
Etienne ANNE <etienne.anne@icloud.com>
2023-10-24 10:48:06 +0200

    fix search bar

---
+++ tools/otoroshi-wasm-manager/server/wasm/foo-0.0.1-dev.wasm/dist/index.js
@@ -0,0 +1,28 @@
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// index.js
+var b429fd3b_310f_4a16_8331_5062162c265f_exports = {};
+__export(b429fd3b_310f_4a16_8331_5062162c265f_exports, {
+  default: () => execute
+});
+module.exports = __toCommonJS(b429fd3b_310f_4a16_8331_5062162c265f_exports);
+function execute() {
+  Host.outputString("{}");
+}
+//# sourceMappingURL=index.js.map
