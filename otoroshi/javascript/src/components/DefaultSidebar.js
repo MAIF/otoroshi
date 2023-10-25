2eefb1566ff61726abf171718e4973855afd2c5c
Zwiterrion <etienne.anne@icloud.com>
2023-10-25 15:40:58 +0200

    add animation

--- otoroshi/javascript/src/components/DefaultSidebar.js
+++ otoroshi/javascript/src/components/DefaultSidebar.js
@@ -30,6 +30,10 @@ export function DefaultSidebar(props) {
   const [shortcuts, setShortcuts] = useState([]);
   const [hightlighted, setHighlighted] = useState();
 
+  const [start, setStart] = useState({ clientY: 0 });
+  const [client, setClient] = useState({ clientY: 0 });
+  const [draggingIndex, setDraggingIndex] = useState(-1);
+
   useEffect(() => {
     reloadStorage()
   }, [])
@@ -59,6 +63,24 @@ export function DefaultSidebar(props) {
     return 0;
   }
 
+  const setPosition = (offset) => {
+    const index = Math.round(draggingIndex + offset)
+
+    console.log(draggingIndex, index)
+
+    const item = shortcuts[draggingIndex];
+    const b = shortcuts[index];
+
+    let newShortcuts = [...shortcuts];
+    newShortcuts[draggingIndex] = b;
+    newShortcuts[index] = item;
+
+    // localStorage.setItem('shortcuts', JSON.stringify(newShortcuts.filter(f => f)))
+
+    setDraggingIndex(index)
+    setShortcuts(newShortcuts)
+  }
+
   const sidebarContext = useContext(SidebarContext);
   const { openedSidebar } = sidebarContext;
 
@@ -67,18 +89,50 @@ export function DefaultSidebar(props) {
   const features = links
     .flatMap(l => l.features.map(f => ({ ...f, title: f.title.toLowerCase() })))
 
-  console.log(hightlighted)
-
   return <>
-    <ul className="nav flex-column nav-sidebar" style={{
-      opacity: !hightlighted ? 1 : .5
-    }}>
+    <ul className="nav flex-column nav-sidebar"
+      style={{
+        opacity: !hightlighted ? 1 : .5,
+      }}
+      onMouseMove={ev => {
+        if (!isNaN(draggingIndex) && draggingIndex !== -1) {
+          if (start.clientY === 0) {
+            setStart({ clientY: ev.clientY })
+          } else {
+            console.log(Math.floor(ev.clientY - start.clientY), Math.floor(ev.clientY - start.clientY) / 32)
+            setClient({ clientY: ev.clientY })
+
+            const offset = Math.floor(ev.clientY - start.clientY) / 32;
+
+            if (offset < -1 || offset >= 1) {
+              const index = Math.round(draggingIndex + offset)
+              if (!(index < 0 || index > shortcuts.length - 1 || (index === draggingIndex))) {
+                setStart({ clientY: ev.clientY })
+                setPosition(offset)
+              }
+            }
+          }
+        }
+      }}
+      onMouseUp={() => {
+        setTimeout(() => {
+          setDraggingIndex(undefined)
+        }, 50) // delay to avoid simple click
+      }}
+    >
       {openedSidebar && <p className="ps-2">Shortcuts</p>}
       {shortcuts
         .map(shortcut => features.find(feat => feat.title.includes(shortcut)))
-        .map(shortcut => {
+        .map((shortcut, initialIndex) => {
           return <SidebarLink
-            shortcuts={shortcuts}
+            key={shortcut.title}
+            dragging={draggingIndex === initialIndex ? ({
+              clientY: client.clientY - start.clientY,
+            }) : undefined}
+            startDragging={clientY => {
+              setStart({ clientY })
+              setDraggingIndex(initialIndex)
+            }}
             removeShortcut={() => removeShortcut(shortcut.title)}
             rootClassName={rootClassName}
             openedSidebar={openedSidebar}
@@ -88,7 +142,7 @@ export function DefaultSidebar(props) {
         })}
     </ul>
     {openedSidebar && <ul className="nav flex-column nav-sidebar me-2" style={{ marginTop: 20 }}>
-      <p>Categories</p>
+      <p className="ps-2">Categories</p>
       <div className="d-flex flex-column">
         {links
           .sort(sortCategory)
@@ -105,7 +159,7 @@ export function DefaultSidebar(props) {
           })}
       </div>
 
-      <li className={`nav-item ${openedSidebar ? "nav-item--open" : ""} pt-3 mt-1`} style={{
+      <li className={`nav-item ${openedSidebar ? "nav-item--open" : ""} mt-3`} style={{
         opacity: !hightlighted ? 1 : .5
       }}>
         <Link
@@ -209,29 +263,48 @@ function SidebarLink({
   icon,
   rootClassName,
   removeShortcut,
+  startDragging,
+  dragging,
   ...props
 }) {
   const path = props.path || props.link;
 
-  return (
-    <li className={`nav-item mt-0 d-flex justify-content-between align-items-center ${openedSidebar ? "nav-item--open" : ""}`}>
-      <Link
-        to={`/${path}`.replace('//', '/')}
-        className={`nav-link ${rootClassName(path)}`}
-        {...createTooltip(text)}
-        onClick={clearSidebar}
-        style={{ flex: 1 }}
-      >
-        <CustomIcon icon={icon} />{" "}
-        <span style={{ marginTop: "4px" }}>
-          {!openedSidebar
-            ? ""
-            : title
-              ? firstLetterUppercase(title)
-              : firstLetterUppercase(path)}
-        </span>
-      </Link>
-      <i className="fas fa-eye nav-item-eye" onClick={removeShortcut} />
-    </li >
-  );
+  return <li
+    className={`nav-item mt-0 d-flex align-items-center ${openedSidebar ? "nav-item--open" : ""}`}
+    draggable={false}
+    style={{
+      position: dragging ? 'asbolute' : 'relative',
+      top: dragging ? dragging.clientY : 'initial',
+      border: dragging ? '1px solid var(--bg-color_level3)' : '2px solid transparent',
+      zIndex: dragging ? 100 : 1,
+      background: dragging ? 'var(--bg-color_level1)' : 'inherit'
+    }}>
+    <i className="fas fa-grip-vertical nav-item-eye d-flex align-items-center m-0"
+      onMouseDown={e => {
+        startDragging(e.clientY)
+      }}
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 6,
+        bottom: 0
+      }} />
+    <Link
+      to={`/${path}`.replace('//', '/')}
+      className={`nav-link ${rootClassName(path)}`}
+      {...createTooltip(text)}
+      onClick={clearSidebar}
+      style={{ flex: 1, marginLeft: 4 }}
+    >
+      <CustomIcon icon={icon} />{" "}
+      <span style={{ marginTop: "4px" }}>
+        {!openedSidebar
+          ? ""
+          : title
+            ? firstLetterUppercase(title)
+            : firstLetterUppercase(path)}
+      </span>
+    </Link>
+    <i className="fas fa-eye nav-item-eye ms-auto" onClick={removeShortcut} />
+  </li>
 }
