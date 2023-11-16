e68e5bde9aa3dcbc029ed094aa9422682c9496c3
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2023-11-16 10:31:15 +0100

    [release 16.10.3] Update site documentation before release

---
+++ docs/manual/snippets/build.sbt
@@ -0,0 +1,2 @@
+libraryDependencies += "fr.maif" %% "otoroshi" % "16.10.3"
+libraryDependencies += "fr.maif" %% "otoroshi" % "16.10.3" classifier "assets"
---
+++ docs/manual/snippets/fetch.sh
@@ -0,0 +1,7 @@
+// #curl
+curl -L -o otoroshi.jar 'https://github.com/MAIF/otoroshi/releases/download/v16.10.3/otoroshi.jar'
+// #curl
+
+// #wget
+wget 'https://github.com/MAIF/otoroshi/releases/download/v16.10.3/otoroshi.jar'
+// #wget
