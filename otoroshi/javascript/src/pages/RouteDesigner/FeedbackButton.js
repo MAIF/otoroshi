4d2638d569fb328da18f3973adcb837817ef6f79
baudelotphilippe <philippe@baudelot.eu>
2023-11-21 16:21:24 +0100

    color save

--- otoroshi/javascript/src/pages/RouteDesigner/FeedbackButton.js
+++ otoroshi/javascript/src/pages/RouteDesigner/FeedbackButton.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 export function FeedbackButton({
-  type = 'primary',
+  type = 'save',
   text,
   icon,
   onPress = () => Promise.resolve(),
