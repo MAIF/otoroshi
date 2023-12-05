fd60799d09698fde38f1c9edea57af08ff061bde
baudelotphilippe <philippe@baudelot.eu>
2023-12-05 15:02:40 +0100

    move pin shortcut (to finish)

--- otoroshi/javascript/src/components/PageTitle.js
+++ otoroshi/javascript/src/components/PageTitle.js
@@ -1,13 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import { Button } from "./Button";
 
-// className='slide-in-blurred-top'
-export default function PageTitle({ title, children, style = {}, className = 'ms-0 mb-3' }) {
+export default function PageTitle({
+  title,
+  children,
+  style = {},
+  className = "ms-0 mb-3",
+}) {
   return (
     <div
       className={`page-header_title d-flex align-item-center justify-content-between ${className}`}
-      style={style}>
-      <h3 className="m-0 align-self-center">{title}</h3>
-      <div className="d-flex align-item-center justify-content-between">{children}</div>
+      style={style}
+    >
+      <div className="d-flex">
+        <h3 className="m-0 align-self-center">{title}</h3>
+        <Button
+          type="quiet"
+          // disabled={shortcutDisabled}
+          title="Add current page to sidebar shortcuts"
+          // onClick={addShortcut}
+          className="ms-3 btn-sm align-self-center"
+        >
+          <i className="fas fa-thumbtack"></i>
+        </Button>
+      </div>
+      <div className="d-flex align-item-center justify-content-between">
+        {children}
+      </div>
     </div>
   );
 }
