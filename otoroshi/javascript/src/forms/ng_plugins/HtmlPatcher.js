51b12fc0fbcfb77e7cccf9e14f04d92fe89eff72
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2024-09-18 10:48:56 +0200

    fix #1980

--- otoroshi/javascript/src/forms/ng_plugins/HtmlPatcher.js
+++ otoroshi/javascript/src/forms/ng_plugins/HtmlPatcher.js
@@ -1,8 +1,26 @@
 export default {
   id: 'cp:otoroshi.next.plugins.NgHtmlPatcher',
   config_schema: {
+    prepend_head: {
+      label: 'Prepend to document head',
+      array: true,
+      type: 'code',
+      props: {
+        mode: 'html',
+        editorOnly: true,
+      },
+    },
     append_head: {
       label: 'Append to document head',
+      array: true,
+      type: 'code',
+      props: {
+        mode: 'html',
+        editorOnly: true,
+      },
+    },
+    prepend_body: {
+      label: 'Prepend to document body',
       array: true,
       type: 'code',
       props: {
@@ -20,5 +38,5 @@ export default {
       },
     },
   },
-  config_flow: ['append_head', 'append_body'],
+  config_flow: ['prepend_head', 'append_head', 'prepend_body', 'append_body'],
 };
