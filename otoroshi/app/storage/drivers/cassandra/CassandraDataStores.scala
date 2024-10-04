25b2318527ac4084e8a3dd7522d8a9a7b1baabbc
zwiterrion <etienne.anne@icloud.com>
2024-10-04 11:33:56 +0200

    add drafts API

--- otoroshi/app/storage/drivers/cassandra/CassandraDataStores.scala
+++ otoroshi/app/storage/drivers/cassandra/CassandraDataStores.scala
@@ -140,6 +140,9 @@ class CassandraDataStores(
   private lazy val _wasmPluginDataStore                  = new KvWasmPluginDataStore(redis, env)
   override def wasmPluginsDataStore: WasmPluginDataStore = _wasmPluginDataStore
 
+  private lazy val _draftDataStore                  = new KvDraftDataStore(redis, env)
+  override def draftsDataStore: DraftDataStore = _draftDataStore
+
   private lazy val _adminPreferencesDatastore              = new AdminPreferencesDatastore(env)
   def adminPreferencesDatastore: AdminPreferencesDatastore = _adminPreferencesDatastore
 
--- otoroshi/app/storage/drivers/lettuce/LettuceDataStore.scala
+++ otoroshi/app/storage/drivers/lettuce/LettuceDataStore.scala
@@ -225,6 +225,9 @@ class LettuceDataStores(
   private lazy val _wasmPluginDataStore                  = new KvWasmPluginDataStore(redis, env)
   override def wasmPluginsDataStore: WasmPluginDataStore = _wasmPluginDataStore
 
+  private lazy val _draftDataStore                  = new KvDraftDataStore(redis, env)
+  override def draftsDataStore: DraftDataStore = _draftDataStore
+
   private lazy val _adminPreferencesDatastore              = new AdminPreferencesDatastore(env)
   def adminPreferencesDatastore: AdminPreferencesDatastore = _adminPreferencesDatastore
 
