f0c5b2575eed699e757e359a5127d47b3d539211
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2024-10-14 22:37:45 +0200

    trying out new stuff - basic structure

--- otoroshi/app/next/models/Api.scala
+++ otoroshi/app/next/models/Api.scala
@@ -3,7 +3,7 @@ package next.models
 import akka.util.ByteString
 import org.joda.time.DateTime
 import otoroshi.models.{EntityLocation, EntityLocationSupport, LoadBalancing}
-import otoroshi.next.models.{NgDomainAndPath, NgPlugins, NgRoute, NgTarget}
+import otoroshi.next.models.{NgClientConfig, NgDomainAndPath, NgPlugins, NgRoute, NgTarget}
 import play.api.libs.json.{Format, JsResult, JsValue}
 
 case class ApiState(started: Boolean, published: Boolean, public: Boolean, deprecated: Boolean)
@@ -19,6 +19,22 @@ case class ApiPredicate()
 case class ApiPlugins(name: String, predicate: ApiPredicate, plugins: NgPlugins)
 
 case class ApiDeploymentRef(ref: String, at: DateTime)
+
+case class ApiDeployment(location: EntityLocation, id: String, apiRef: String, owner: String, at: DateTime) extends EntityLocationSupport {
+  override def internalId: String = id
+  override def json: JsValue = ApiDeployment.format.writes(this)
+  override def theName: String = id
+  override def theDescription: String = id
+  override def theTags: Seq[String] = Seq.empty
+  override def theMetadata: Map[String, String] = Map.empty
+}
+
+object ApiDeployment {
+  val format = new Format[ApiDeployment] {
+    override def reads(json: JsValue): JsResult[ApiDeployment] = ???
+    override def writes(o: ApiDeployment): JsValue = ???
+  }
+}
 
 sealed trait ApiSpecification {
   def content: JsValue
@@ -38,7 +54,7 @@ object ApiPage {
   case class ApiPageLeaf(path: String, name: String, content: ByteString)
 }
 
-case class ApiDocumentation(specification: ApiSpecification, home: ApiPage, pages: Seq[ApiPage])
+case class ApiDocumentation(specification: ApiSpecification, home: ApiPage, pages: Seq[ApiPage], metadata: Map[String, String])
 
 trait ApiBlueprint {
   def name: String
@@ -48,7 +64,68 @@ object ApiBlueprint {
   case class REST() extends ApiBlueprint { def name: String = "REST" }
   case class GraphQL() extends ApiBlueprint { def name: String = "GraphQL" }
   case class gRPC() extends ApiBlueprint { def name: String = "gRPC" }
+  case class Http() extends ApiBlueprint { def name: String = "Http" }
+  case class Websocket() extends ApiBlueprint { def name: String = "Websocket" }
 }
+
+case class ApiConsumer(name: String, description: String, autoValidation: Boolean, kind: ApiConsumerKind, status: ApiConsumerStatus, subscriptions: Seq[ApiConsumerSubscriptionRef])
+
+case class ApiConsumerSubscriptionDates(
+  created_at: DateTime,
+  processed_at: DateTime,
+  started_at: DateTime,
+  ending_at: DateTime,
+  closed_at: DateTime,
+)
+
+case class ApiConsumerSubscription(
+  location: EntityLocation,
+  id: String,
+  name: String,
+  description: String,
+  tags: Seq[String],
+  metadata: Map[String, String],
+  enabled: Boolean,
+  dates: ApiConsumerSubscriptionDates,
+  ownerRef: String,
+  consumerRef: Option[String],
+  kind: ApiConsumerKind,
+  tokenRefs: Seq[String] // ref to apikey, cert, etc
+) extends EntityLocationSupport {
+  override def internalId: String = id
+  override def json: JsValue = ApiConsumerSubscription.format.writes(this)
+  override def theName: String = name
+  override def theDescription: String = description
+  override def theTags: Seq[String] = tags
+  override def theMetadata: Map[String, String] = metadata
+}
+
+object ApiConsumerSubscription {
+  val format = new Format[ApiConsumerSubscription] {
+    override def reads(json: JsValue): JsResult[ApiConsumerSubscription] = ???
+    override def writes(o: ApiConsumerSubscription): JsValue = ???
+  }
+}
+
+case class ApiConsumerSubscriptionRef(ref: String)
+
+trait ApiConsumerKind
+object ApiConsumerKind {
+  case object Apikey extends ApiConsumerKind
+  case object Mtls extends ApiConsumerKind
+  case object Keyless extends ApiConsumerKind
+  case object OAuth2 extends ApiConsumerKind
+  case object JWT extends ApiConsumerKind
+}
+
+trait ApiConsumerStatus
+object ApiConsumerStatus {
+  case object Staging extends ApiConsumerStatus
+  case object Published extends ApiConsumerStatus
+  case object Deprecated extends ApiConsumerStatus
+  case object Closed extends ApiConsumerStatus
+}
+case class ApiBackendClient(name: String, client: NgClientConfig)
 
 case class Api(
    location: EntityLocation,
@@ -60,11 +137,15 @@ case class Api(
    version: String,
    // or versions: Seq[ApiVersion] with ApiVersion being the following ?
    //// ApiVersion
+   debugFlow: Boolean,
+   capture: Boolean,
+   exportReporting: Boolean,
    state: ApiState,
    blueprint: ApiBlueprint,
    routes: Seq[ApiRoute],
    backends: Seq[ApiBackend],
    plugins: Seq[ApiPlugins],
+   clients: Seq[ApiBackendClient],
    documentation: ApiDocumentation,
    consumers: Seq[ApiConsumer],
    deployments: Seq[ApiDeploymentRef],
