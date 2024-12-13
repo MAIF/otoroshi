de98c76353011848529fa7a30020c3664beffe55
zwiterrion <etienne.anne@icloud.com>
2024-12-13 16:56:22 +0100

    add search bar tests

--- otoroshi/javascript/playwright.config.js
+++ otoroshi/javascript/playwright.config.js
@@ -3,7 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     testDir: './tests',
     fullyParallel: true,
-    timeout: 12000,
+    timeout: 4000,
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
