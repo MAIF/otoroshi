2d907feb45224a643660668dddb1392540218da5
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2025-01-23 10:42:34 +0100

    Add mapAsync to async utils

--- otoroshi/app/utils/async.scala
+++ otoroshi/app/utils/async.scala
@@ -1,6 +1,35 @@
 package otoroshi.utils
 
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success}
+
 class AsyncUtils {
 
+  def mapAsync[A](items: Seq[Future[A]])(implicit ec: ExecutionContext): Future[Seq[A]] = {
 
+    val promise = Promise[Seq[A]]()
+    var results = Seq.empty[A]
+
+    def next(futures: Seq[Future[A]]): Unit = {
+      if (futures.isEmpty) {
+        promise.trySuccess(results)
+      } else {
+        val head = futures.head
+        head.andThen {
+          case Failure(e) => promise.tryFailure(e)
+          case Success(value) => {
+            results = results :+ value
+            if (futures.size == 1) {
+              promise.trySuccess(results)
+            } else {
+              next(futures.tail)
+            }
+          }
+        }
+      }
+    }
+
+    next(items)
+    promise.future
+  }
 }
