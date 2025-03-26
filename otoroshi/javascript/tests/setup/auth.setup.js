740a863b5443484f1a919c56de5726bc736cb48e
zwiterrion <etienne.anne@icloud.com>
2025-03-26 16:38:16 +0100

    better playwright integration

--- otoroshi/javascript/tests/setup/auth.setup.js
+++ otoroshi/javascript/tests/setup/auth.setup.js
@@ -1,11 +1,26 @@
 import { test as setup, expect } from '@playwright/test';
 import path from 'path';
 
-const authFile = path.join(__dirname, '../playwright/.auth/user.json');
+const userAuthFile = path.join(__dirname, '../playwright/.auth/user.json');
+const adminAuthFile = path.join(__dirname, '../playwright/.auth/admin.json');
 
 setup('authenticate', async ({ page }) => {
-    // Perform authentication steps. Replace these actions with your own.
     await page.goto('/');
+    await page.getByRole('button', { name: 'Login', exact: true }).click();
+    await page.locator('input[name="email"]').click();
+    await page.locator('input[name="email"]').fill('tester@otoroshi.io');
+    await page.locator('input[name="email"]').press('Tab');
+    await page.locator('input[name="password"]').fill('password');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await page.waitForURL('/bo/dashboard');
+
+    await expect(page.locator('#content-scroll-container img')).toBeVisible();
+
+    await page.context().storageState({ path: userAuthFile });
+
+    // await page.goto('/backoffice/auth0/logout');
+    await page.goto('/bo/simple/login');
     await page.getByRole('button', { name: 'Login', exact: true }).click();
     await page.locator('input[name="email"]').click();
     await page.locator('input[name="email"]').fill('admin@otoroshi.io');
@@ -17,5 +32,5 @@ setup('authenticate', async ({ page }) => {
 
     await expect(page.locator('#content-scroll-container img')).toBeVisible();
 
-    await page.context().storageState({ path: authFile });
+    await page.context().storageState({ path: adminAuthFile });
 });
