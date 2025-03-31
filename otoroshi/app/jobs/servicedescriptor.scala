23e222ad32307d26a167dda8333c29138067755a
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2025-03-31 12:56:32 +0200

    migration job (SD -> Route) disabled by default for #2125

--- otoroshi/app/jobs/servicedescriptor.scala
+++ otoroshi/app/jobs/servicedescriptor.scala
@@ -1,12 +1,16 @@
 package otoroshi.jobs
 
+import akka.stream.scaladsl.{Sink, Source}
 import otoroshi.env.Env
+import otoroshi.next.models.NgRoute
 import otoroshi.next.plugins.api.NgPluginCategory
 import otoroshi.script._
 import otoroshi.utils.syntax.implicits._
 import play.api.Logger
-import play.api.libs.json.JsObject
+import play.api.libs.json.{JsArray, JsObject, Json}
 
+import java.io.File
+import java.nio.file.Files
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
@@ -56,6 +60,68 @@ class ServiceDescriptorUsageWarning extends Job {
         env.logger.warn(s"-------------------------------------------------------------------------")
         env.logger.warn("")
       }
+    }
+  }
+}
+
+class ServiceDescriptorMigrationJob extends Job {
+
+  private val logger = Logger("otoroshi-jobs-service-descriptor-migration-job")
+
+  override def categories: Seq[NgPluginCategory] = Seq.empty
+
+  override def uniqueId: JobId = JobId("io.otoroshi.core.jobs.ServiceDescriptorMigrationJob")
+
+  override def name: String = "Otoroshi service descriptor migration job"
+
+  override def defaultConfig: Option[JsObject] = None
+
+  override def description: Option[String] =
+    s"""This job will transform all ServiceDescriptors into routes""".stripMargin.some
+
+  override def jobVisibility: JobVisibility = JobVisibility.Internal
+
+  override def kind: JobKind = JobKind.ScheduledOnce
+
+  override def starting: JobStarting = JobStarting.Automatically
+
+  override def instantiation(ctx: JobContext, env: Env): JobInstantiation =
+    JobInstantiation.OneInstancePerOtoroshiCluster
+
+  override def initialDelay(ctx: JobContext, env: Env): Option[FiniteDuration] = 10.seconds.some
+
+  override def interval(ctx: JobContext, env: Env): Option[FiniteDuration] = None
+
+  override def predicate(ctx: JobContext, env: Env): Option[Boolean] = None
+
+  override def jobRun(ctx: JobContext)(implicit env: Env, ec: ExecutionContext): Future[Unit] = {
+    if (env.configuration.getOptional[Boolean]("otoroshi.service-descriptors-migration-job.enabled").getOrElse(false)) {
+      env.logger.warn("Running full Service Descriptors migration !!!")
+      env.logger.warn("")
+      env.datastores.serviceDescriptorDataStore.findAll(force = true).flatMap { descriptors =>
+        val backup = new File("./service-descriptors-backup.json")
+        env.logger.warn(s" - writing a backup to '${backup.getAbsolutePath}'")
+        env.logger.warn("")
+        val json = JsArray(descriptors.map(_.json))
+        Files.writeString(backup.toPath, json.stringify)
+        Source(descriptors.toList)
+          .mapAsync(1) { descriptor =>
+            env.logger.warn(s" - migrating '${descriptor.name}' ...")
+            val route = NgRoute.fromServiceDescriptor(descriptor, debug = false)
+            route.save().flatMap { _ =>
+              env.datastores.serviceDescriptorDataStore.delete(descriptor).map { _ =>
+                env.logger.warn(s" - migrating '${descriptor.name}' - OK")
+              }
+            }.recover {
+              case t: Throwable => env.logger.error(s"error while migrating '${descriptor.name}'", t)
+            }
+          }.runWith(Sink.ignore)(env.otoroshiMaterializer)
+          .andThen {
+            case _ => env.logger.warn("migration done !")
+          }
+      }.map(_ => ())
+    } else {
+      ().future
     }
   }
 }
