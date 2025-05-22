9be60b229e6cf09fa8267535c31127c79d82fff8
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2025-05-22 17:31:59 +0200

    add description for debug

--- otoroshi/app/next/workflow/api.scala
+++ otoroshi/app/next/workflow/api.scala
@@ -120,6 +120,7 @@ object WorkflowFunction {
 trait Node {
   def json: JsObject
   def id: String                = json.select("id").asOptString.getOrElse(ULID.random().toLowerCase)
+  def description: String       = json.select("description").asOptString.getOrElse("")
   def kind: String              = json.select("kind").asString
   def enabled: Boolean          = json.select("enabled").asOptBoolean.getOrElse(true)
   def result: Option[String]    = json.select("result").asOptString
