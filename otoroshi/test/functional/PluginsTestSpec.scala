7890be77c5087e34b7c94753ea2861d1dc65a51d
zwiterrion <etienne.anne@icloud.com>
2025-10-09 11:26:50 +0200

    task(#2268): create test for Query param transformer

--- otoroshi/test/functional/PluginsTestSpec.scala
+++ otoroshi/test/functional/PluginsTestSpec.scala
@@ -1981,5 +1981,43 @@ class PluginsTestSpec extends OtoroshiSpec with BeforeAndAfterAll {
       resp.contains("HTTP/1.0 503 Service Unavailable") mustBe true
       deleteOtoroshiRoute(route).await()
     }
+
+    "Query param transformer" in {
+      val route = createLocalRoute(
+        Seq(
+          NgPluginInstance(
+            plugin = NgPluginHelper.pluginId[OverrideHost]
+          ),
+          NgPluginInstance(
+            plugin = NgPluginHelper.pluginId[QueryTransformer],
+            config = NgPluginInstanceConfig(
+              QueryTransformerConfig(
+                remove = Seq("foo"),
+                rename = Map("bar" -> "baz"),
+                add = Map("new_query" -> "value")
+              ).json.as[JsObject]
+            )
+          )
+        ),
+        result = req => {
+          Json.obj("query_params" -> req.uri.query().toMap)
+        })
+
+      val resp = ws
+        .url(s"http://127.0.0.1:$port/api/?foo=bar&bar=foo")
+        .withHttpHeaders(
+          "Host" -> route.frontend.domains.head.domain,
+        )
+        .get()
+        .futureValue
+
+      resp.status mustBe Status.OK
+      Json.parse(resp.body).selectAsObject("query_params") mustEqual Json.obj(
+        "baz" -> "foo",
+        "new_query" -> "value"
+      )
+
+      deleteOtoroshiRoute(route).await()
+    }
   }
 }
