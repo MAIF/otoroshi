d85253f1cc1efab0876dddb43d8439477cac92bd
zwiterrion <etienne.anne@icloud.com>
2025-10-09 16:36:06 +0200

    task(#2268): create test for Read only requests

--- otoroshi/test/functional/PluginsTestSpec.scala
+++ otoroshi/test/functional/PluginsTestSpec.scala
@@ -2019,5 +2019,47 @@ class PluginsTestSpec extends OtoroshiSpec with BeforeAndAfterAll {
 
       deleteOtoroshiRoute(route).await()
     }
+
+    "Read only requests" in {
+       val route = createRequestOtoroshiIORoute(
+         Seq(
+          NgPluginInstance(NgPluginHelper.pluginId[OverrideHost]),
+          NgPluginInstance(NgPluginHelper.pluginId[ReadOnlyCalls])
+        )
+       )
+
+      def req() = ws
+          .url(s"http://127.0.0.1:$port/api")
+          .withHttpHeaders(
+            "Host" -> route.frontend.domains.head.domain,
+          )
+
+      req()
+          .get()
+          .futureValue
+          .status mustBe Status.OK
+
+      req()
+          .head()
+          .futureValue
+          .status mustBe Status.OK
+
+      req()
+          .options()
+          .futureValue
+          .status mustBe Status.NO_CONTENT
+
+      req()
+          .post("")
+          .futureValue
+          .status mustBe Status.METHOD_NOT_ALLOWED
+
+      req()
+          .patch("")
+          .futureValue
+          .status mustBe Status.METHOD_NOT_ALLOWED
+
+      deleteOtoroshiRoute(route).await()
+    }
   }
 }
