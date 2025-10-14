a615084923d321fbe006c97734afe712c3eb010c
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2025-10-14 16:10:11 +0200

    fix #2282

--- otoroshi/app/env/Env.scala
+++ otoroshi/app/env/Env.scala
@@ -1249,7 +1249,8 @@ class Env(
     localHost = s"127.0.0.1:$port",
     forceHttps = false,
     additionalHeaders = Map(
-      "Host" -> backOfficeDescriptorHostHeader
+      "Host" -> backOfficeDescriptorHostHeader,
+      "X-Forwarded-For" -> backOfficeDescriptorHostHeader,
     ),
     publicPatterns = Seq("/health", "/metrics"),
     allowHttp10 = true,
