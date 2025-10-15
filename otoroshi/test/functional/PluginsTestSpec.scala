557e34de89b0a2cbb34ef52d33159f33c30dd2d6
zwiterrion <etienne.anne@icloud.com>
2025-10-15 10:19:53 +0200

    task(#2268): create test for Custom quotas

--- otoroshi/test/functional/PluginsTestSpec.scala
+++ otoroshi/test/functional/PluginsTestSpec.scala
@@ -3003,7 +3003,133 @@ class PluginsTestSpec extends OtoroshiSpec with BeforeAndAfterAll {
         resp.status mustBe Status.REQUEST_ENTITY_TOO_LARGE
       }
 
+      deleteOtoroshiRoute(route).futureValue
+    }
 
+    "Custom quotas (per route)" in {
+      val route = createRequestOtoroshiIORoute(
+        Seq(
+          NgPluginInstance(
+            plugin = NgPluginHelper.pluginId[OverrideHost]
+          ),
+          NgPluginInstance(
+            plugin = NgPluginHelper.pluginId[NgCustomQuotas],
+            config = NgPluginInstanceConfig(
+              NgCustomQuotasConfig(
+                dailyQuota = 1,
+                monthlyQuota = 1,
+                perRoute = true,
+                global = false,
+                group = None,
+                expression = "${req.headers.foo}"
+              ).json.as[JsObject]
+            )
+          )
+        )
+      )
+
+      def call(value: String) = {
+        ws
+          .url(s"http://127.0.0.1:$port/api")
+          .withHttpHeaders(
+            "Host" -> PLUGINS_HOST,
+            "foo" -> value
+          )
+          .get()
+          .futureValue
+      }
+
+      {
+        val resp = call("bar")
+        resp.status mustBe Status.OK
+      }
+
+      {
+        val resp = call("bar")
+        resp.status mustBe Status.FORBIDDEN
+      }
+
+      {
+        val resp = call("baz")
+        resp.status mustBe Status.OK
+      }
+
+
+      deleteOtoroshiRoute(route).futureValue
+    }
+
+    "Custom quotas (global)" in {
+      val route = createRequestOtoroshiIORoute(
+        Seq(
+          NgPluginInstance(
+            plugin = NgPluginHelper.pluginId[OverrideHost]
+          ),
+          NgPluginInstance(
+            plugin = NgPluginHelper.pluginId[NgCustomQuotas],
+            config = NgPluginInstanceConfig(
+              NgCustomQuotasConfig(
+                dailyQuota = 2,
+                monthlyQuota = 2,
+                perRoute = false,
+                global = true,
+                group = None,
+                expression = "${req.headers.foo}"
+              ).json.as[JsObject]
+            )
+          )
+        ),
+        id = IdGenerator.uuid
+      )
+
+      val secondRoute = createRequestOtoroshiIORoute(
+        Seq(
+          NgPluginInstance(
+            plugin = NgPluginHelper.pluginId[OverrideHost]
+          ),
+          NgPluginInstance(
+            plugin = NgPluginHelper.pluginId[NgCustomQuotas],
+            config = NgPluginInstanceConfig(
+              NgCustomQuotasConfig(
+                dailyQuota = 2,
+                monthlyQuota = 2,
+                perRoute = false,
+                global = true,
+                group = None,
+                expression = "${req.headers.foo}"
+              ).json.as[JsObject]
+            )
+          )
+        ),
+        id = IdGenerator.uuid
+      )
+
+      def call(route: NgRoute) = {
+        ws
+          .url(s"http://127.0.0.1:$port/api")
+          .withHttpHeaders(
+            "Host" -> route.frontend.domains.head.domain,
+            "foo" -> "bar"
+          )
+          .get()
+          .futureValue
+      }
+
+      {
+        val resp = call(route)
+        resp.status mustBe Status.OK
+      }
+
+      {
+        val resp = call(secondRoute)
+        resp.status mustBe Status.OK
+      }
+
+      {
+        val resp = call(route)
+        resp.status mustBe Status.FORBIDDEN
+      }
+
+      deleteOtoroshiRoute(secondRoute).futureValue
       deleteOtoroshiRoute(route).futureValue
     }
   }
