74045ce1bbf2f4b68d0cf960f803f58e088dc6f8
zwiterrion <etienne.anne@icloud.com>
2025-10-31 14:25:52 +0100

    task(#2268): create test for Websocket JQ Transformer

--- otoroshi/test/functional/PluginsTestSpec.scala
+++ otoroshi/test/functional/PluginsTestSpec.scala
@@ -3763,5 +3763,74 @@ class PluginsTestSpec extends OtoroshiSpec with BeforeAndAfterAll {
       http.shutdownAllConnectionPools()
       deleteOtoroshiRoute(route).futureValue
     }
+
+    "Websocket JQ Transformer" in {
+      implicit val http: HttpExt = Http()(system)
+
+      val backend = new WebsocketBackend(
+        callback = text => TextMessage(Json.obj("message" -> text).stringify)
+      ).await()
+
+      val route = createLocalRoute(
+        frontendPath = "/",
+        plugins = Seq(
+          NgPluginInstance(
+            plugin = NgPluginHelper.pluginId[OverrideHost]
+          ),
+          NgPluginInstance(
+            plugin = NgPluginHelper.pluginId[JqWebsocketMessageTransformer],
+            config = NgPluginInstanceConfig(
+              JqWebsocketMessageTransformerConfig(
+                requestFilter = ".name",
+                responseFilter = "."
+              ).json.as[JsObject]
+            )
+          )
+        ),
+        target = NgTarget(
+          hostname = "127.0.0.1",
+          port = backend.backendPort,
+          id = "local.target",
+          tls = false
+        ).some
+      )
+
+      val messagesPromise = Promise[Int]()
+      val counter = new AtomicInteger(0)
+
+      val printSink: Sink[Message, Future[Done]] = Sink.foreach { message =>
+        println(message)
+        counter.incrementAndGet()
+        if(counter.get == 1)
+          messagesPromise.trySuccess(counter.get)
+        else if(counter.get > 1)
+          messagesPromise.tryFailure(new RuntimeException("Failure, but lost track of exception :-("))
+      }
+
+      val messages = List(
+        TextMessage(Json.obj("name" -> "barbarbarbar").stringify),
+        TextMessage(Json.obj("foo" -> "barbarbarbar").stringify),
+        TextMessage(Json.obj("foo" -> "barbarbarbarbar").stringify),
+        TextMessage(Json.obj("foo" -> "barbarbarbarbar").stringify),
+      )
+
+      val clientSource: Source[TextMessage, NotUsed] = Source(messages)
+        .throttle(1, 200.millis)
+
+      val (_, _) = http.singleWebSocketRequest(
+        WebSocketRequest(s"ws://127.0.0.1:$port/")
+          .copy(extraHeaders = List(Host(route.frontend.domains.head.domainLowerCase))),
+        Flow
+          .fromSinkAndSourceMat(printSink, clientSource)(Keep.both)
+          .watchTermination()(Keep.both)
+      )
+
+      val yesMessagesCounter = messagesPromise.future.futureValue(Timeout(Span(25, Seconds)))
+      yesMessagesCounter mustBe 1
+
+      backend.await()
+      http.shutdownAllConnectionPools()
+      deleteOtoroshiRoute(route).futureValue
+    }
   }
 }
