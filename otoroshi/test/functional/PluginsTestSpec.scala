cdc8b3aa30a2a008965a40a01a825d94f330b1ad
zwiterrion <etienne.anne@icloud.com>
2025-10-31 11:19:57 +0100

    task(#2268): create test for Websocket size validator

--- otoroshi/test/functional/PluginsTestSpec.scala
+++ otoroshi/test/functional/PluginsTestSpec.scala
@@ -3473,11 +3473,7 @@ class PluginsTestSpec extends OtoroshiSpec with BeforeAndAfterAll {
 
       closed.onComplete {
         case Success(_) => println("WebSocket connection closed normally")
-        case Failure(ex) => {
-          println("fini")
-          println(ex.getMessage)
-          ex.getMessage.contains("Stopping now") mustBe true
-        }
+        case Failure(ex) => ex.getMessage.contains("Stopping now") mustBe true
       }
       val yesMessagesCounter = messagesPromise.future.futureValue(Timeout(Span(20, Seconds)))
       yesMessagesCounter mustBe 2
@@ -3614,6 +3610,76 @@ class PluginsTestSpec extends OtoroshiSpec with BeforeAndAfterAll {
 
       val yesMessagesCounter = messagesPromise.future.futureValue(Timeout(Span(20, Seconds)))
       yesMessagesCounter mustBe 2
+
+      backend.await()
+      http.shutdownAllConnectionPools()
+      deleteOtoroshiRoute(route).futureValue
+    }
+
+    "Websocket size validator" in {
+      implicit val http: HttpExt = Http()(system)
+
+      val backend = new WebsocketBackend().await()
+
+      val route = createLocalRoute(
+        frontendPath = "/",
+        plugins = Seq(
+          NgPluginInstance(
+            plugin = NgPluginHelper.pluginId[OverrideHost]
+          ),
+          NgPluginInstance(
+            plugin = NgPluginHelper.pluginId[WebsocketSizeValidator],
+            config = NgPluginInstanceConfig(
+              WebsocketSizeValidatorConfig(
+                clientMaxPayload = 10,
+                upstreamMaxPayload = 20,
+                rejectStrategy = RejectStrategy.Drop
+              ).json.as[JsObject]
+            )
+          )
+        ),
+        target = NgTarget(
+          hostname = "127.0.0.1",
+          port = backend.backendPort,
+          id = "local.target",
+          tls = false
+        ).some
+      )
+
+      val messagesPromise = Promise[Int]()
+      val counter = new AtomicInteger(0)
+
+      val printSink: Sink[Message, Future[Done]] = Sink.foreach { message =>
+        counter.incrementAndGet()
+        if(counter.get == 4)
+          messagesPromise.trySuccess(counter.get)
+        else if(counter.get > 4)
+          messagesPromise.tryFailure(new RuntimeException("Failure, but lost track of exception :-("))
+      }
+
+      val messages = List(
+        TextMessage("name"),
+        TextMessage("name"),
+        TextMessage(Json.obj("foo" -> "barbarbarbar").stringify),
+        TextMessage(Json.obj("foo" -> "barbarbarbarbar").stringify),
+        TextMessage("nothing"),
+        TextMessage("nothing"),
+        TextMessage(Json.obj("foo" -> "barbarbarbarbar").stringify),
+      )
+
+      val clientSource: Source[TextMessage, NotUsed] = Source(messages)
+        .throttle(1, 200.millis)
+
+      val (_, _) = http.singleWebSocketRequest(
+        WebSocketRequest(s"ws://127.0.0.1:$port/")
+          .copy(extraHeaders = List(Host(route.frontend.domains.head.domainLowerCase))),
+        Flow
+          .fromSinkAndSourceMat(printSink, clientSource)(Keep.both)
+          .watchTermination()(Keep.both)
+      )
+
+      val yesMessagesCounter = messagesPromise.future.futureValue(Timeout(Span(25, Seconds)))
+      yesMessagesCounter mustBe 4
 
       backend.await()
       http.shutdownAllConnectionPools()
