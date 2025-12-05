e11fc3a208d48605e4cac473780c3b6801014b50
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2025-12-05 22:03:51 +0100

    fix #2370

--- otoroshi/app/next/plugins/identity.scala
+++ otoroshi/app/next/plugins/identity.scala
@@ -2,6 +2,7 @@ package otoroshi.next.plugins
 
 import akka.stream.Materializer
 import otoroshi.env.Env
+import otoroshi.gateway.GatewayRequestHandler
 import otoroshi.next.plugins.api._
 import otoroshi.next.proxy.NgProxyEngineError
 import otoroshi.utils.syntax.implicits._
@@ -87,6 +88,42 @@ class ConsumerEndpoint extends NgBackendCall {
           NgPluginHttpResponse.fromResult(Results.Ok(user.lightJson.asObject ++ Json.obj("access_type" -> "session"))),
           None
         ).rightf
+      }
+    }
+  }
+}
+
+class UserLogoutEndpoint extends NgBackendCall {
+
+  override def steps: Seq[NgStep]                          = Seq(NgStep.CallBackend)
+  override def categories: Seq[NgPluginCategory]           = Seq(NgPluginCategory.Authentication)
+  override def visibility: NgPluginVisibility              = NgPluginVisibility.NgUserLand
+  override def multiInstance: Boolean                      = true
+  override def core: Boolean                               = true
+  override def name: String                                = "User logout endpoint"
+  override def description: Option[String]                 = "This plugin logout the current user".some
+  override def defaultConfigObject: Option[NgPluginConfig] = None
+  override def useDelegates: Boolean                       = false
+  override def noJsForm: Boolean                           = true
+
+  override def callBackend(
+    ctx: NgbBackendCallContext,
+    delegates: () => Future[Either[NgProxyEngineError, BackendCallResponse]]
+  )(implicit
+    env: Env,
+    ec: ExecutionContext,
+    mat: Materializer
+  ): Future[Either[NgProxyEngineError, BackendCallResponse]] = {
+    ctx.user match {
+      case None       =>
+        BackendCallResponse(
+          NgPluginHttpResponse.fromResult(Results.NoContent),
+          None
+        ).rightf
+      case Some(_) => {
+        GatewayRequestHandler.removePrivateAppsCookies(ctx.route, ctx.rawRequest, ctx.attrs).map { result =>
+          BackendCallResponse(NgPluginHttpResponse.fromResult(result), None).right
+        }
       }
     }
   }
