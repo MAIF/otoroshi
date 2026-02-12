2733eadbe58cde6f30e9be5366f3ec8656f0e8c5
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2026-02-12 17:04:25 +0100

    update

--- scripts/gateway-api-test/10-conformance.sh
+++ scripts/gateway-api-test/10-conformance.sh
@@ -3,10 +3,4 @@ cd gateway-api
 
 go test ./conformance -timeout 30m0s -v -run TestConformance -args -debug \
     --gateway-class=gateway-conformance \
-    --supported-features=Gateway,HTTPRoute,GRPCRoute \
-    --organization=CloudAPIM \
-    --project=otoroshi \
-    --url=https://github.com/MAIF/otoroshi \
-    --version=17.13.0 \
-    --contact=contact@cloud-apim.com \
-    --report-output=./report.txt
+    --supported-features=Gateway,HTTPRoute,GRPCRoute 
