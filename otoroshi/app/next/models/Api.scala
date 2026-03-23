cbcaf13bbb77c694d83cffb620bccfbc7417c2aa
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2026-03-23 10:24:54 +0100

    bad match handling

--- otoroshi/app/next/models/Api.scala
+++ otoroshi/app/next/models/Api.scala
@@ -647,6 +647,7 @@ case class ApiDocumentationPlan(raw: JsObject) {
     case "published"  => ApiPlanStatus.Published
     case "deprecated" => ApiPlanStatus.Deprecated
     case "closed"     => ApiPlanStatus.Closed
+    case _            => ApiPlanStatus.Staging
   }
   lazy val tags: Seq[String]                                                        = raw.select("tags").asOpt[Seq[String]].getOrElse(Seq.empty)
   lazy val metadata: Map[String, String]                                            = raw.select("metadata").asOpt[Map[String, String]].getOrElse(Map.empty)
