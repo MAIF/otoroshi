31bebbe842fb9759d23bfa64378c5f1932d5d294
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2026-04-01 20:25:51 +0200

    update doc

--- manual/next/build.sh
+++ manual/next/build.sh
@@ -3,7 +3,7 @@
 nvm use 24
 npm run build
 npm run build-root
-rm -rf ../docs/manual
-rm -rf ../docs/manual-root
-cp -r build ../docs/manual
-cp -r build-root ../docs/manual-root
+rm -rf ../../docs/manual
+rm -rf ../../docs/manual-root
+cp -r build ../../docs/manual
+cp -r build-root ../../docs/manual-root
