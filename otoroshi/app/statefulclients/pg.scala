9fa7c8b47906fd06d3c0e1289d730b38df2f1457
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2026-04-09 09:07:18 +0200

    change types for pg config for #2436 in pg.scala

--- otoroshi/app/statefulclients/pg.scala
+++ otoroshi/app/statefulclients/pg.scala
@@ -1,7 +1,7 @@
 package otoroshi.statefulclients
 
-import io.vertx.pgclient.{PgConnectOptions, PgPool}
-import io.vertx.sqlclient.PoolOptions
+import io.vertx.pgclient.PgConnectOptions
+import io.vertx.sqlclient.{Pool, PoolOptions}
 import otoroshi.utils.syntax.implicits._
 import play.api.libs.json.JsObject
 
@@ -14,24 +14,24 @@ object PgStatefulClientConfig {
   )
 }
 
-case class PgStatefulClientConfig(uri: String, poolSize: Int = 10) extends StatefulClientConfig[PgPool] {
+case class PgStatefulClientConfig(uri: String, poolSize: Int = 10) extends StatefulClientConfig[Pool] {
 
   private val open = new AtomicBoolean(false)
 
-  override def start(): PgPool = {
+  override def start(): Pool = {
     val connectOptions = PgConnectOptions.fromUri(uri)
     val poolOptions = new PoolOptions().setMaxSize(poolSize)
-    val pool = PgPool.pool(connectOptions, poolOptions)
+    val pool = Pool.pool(connectOptions, poolOptions)
     open.set(true)
     pool
   }
 
-  override def stop(client: PgPool): Unit = {
+  override def stop(client: Pool): Unit = {
     open.set(false)
     client.close()
   }
 
-  override def isOpen(client: PgPool): Boolean = open.get()
+  override def isOpen(client: Pool): Boolean = open.get()
 
   override def sameConfig(other: StatefulClientConfig[_]): Boolean = other match {
     case p: PgStatefulClientConfig => p.uri == uri && p.poolSize == poolSize
