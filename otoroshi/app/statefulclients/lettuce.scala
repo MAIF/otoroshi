cc2e6daf03e771cb54b0e2e5a76b71add98c0ba7
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2026-05-07 17:16:50 +0200

    cleanup

--- otoroshi/app/statefulclients/lettuce.scala
+++ otoroshi/app/statefulclients/lettuce.scala
@@ -64,10 +64,10 @@ case class DistributedRateLimiterLettuceStatefulClientConfig(uri: String) extend
   }
 }
 
-object LettuceClusterStatefulClientConfig {
-  def apply(obj: JsObject) =
-    new LettuceClusterStatefulClientConfig(obj.select("uris").asOpt[Seq[String]].getOrElse(Seq.empty))
-}
+// object LettuceClusterStatefulClientConfig {
+//   def apply(obj: JsObject) =
+//     new LettuceClusterStatefulClientConfig(obj.select("uris").asOpt[Seq[String]].getOrElse(Seq.empty))
+// }
 
 case class LettuceClusterStatefulClientConfig(uris: Seq[String])
     extends StatefulClientConfig[StatefulRedisClusterConnection[String, ByteString]] {
